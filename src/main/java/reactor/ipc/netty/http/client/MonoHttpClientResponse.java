f064c1034d060613371c527ac6bac3e7ca32c235
Stephane Maldini <smaldini@pivotal.io>
2016-11-21 21:35:03 +0000

    toString more

--- src/main/java/reactor/ipc/netty/http/client/MonoHttpClientResponse.java
+++ src/main/java/reactor/ipc/netty/http/client/MonoHttpClientResponse.java
@@ -18,6 +18,7 @@ package reactor.ipc.netty.http.client;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -25,13 +26,14 @@ import java.util.function.Predicate;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpMethod;
-import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.util.AsciiString;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
+import reactor.ipc.netty.NettyInbound;
+import reactor.ipc.netty.NettyOutbound;
 
 /**
  * @author Stephane Maldini
@@ -67,41 +69,61 @@ final class MonoHttpClientResponse extends Mono<HttpClientResponse> {
 		ReconnectableBridge bridge = new ReconnectableBridge();
 		bridge.activeURI = startURI;
 
-		Mono.defer(() -> parent.client.newHandler((in, out) -> {
-					try {
-						URI uri = bridge.activeURI;
-						HttpClientOperations ch = (HttpClientOperations) in;
-						ch.getNettyRequest()
-						  .setUri(uri.getPath() + (uri.getQuery() == null ? "" :
-								  "?" + uri.getRawQuery()))
-						  .setMethod(method)
-						  .setProtocolVersion(HttpVersion.HTTP_1_1)
-						  .headers()
-						  .add(HttpHeaderNames.HOST, uri.getHost())
-						  .add(HttpHeaderNames.ACCEPT, ALL);
-
-						if (method == HttpMethod.GET || method == HttpMethod.HEAD) {
-							ch.disableChunkedTransfer();
-						}
-
-						if (handler != null) {
-							return handler.apply(ch);
-						}
-						else {
-							ch.disableChunkedTransfer();
-							return ch.sendHeaders();
-						}
-					}
-					catch (Throwable t) {
-						return Mono.error(t);
-					}
-				},
+		Mono.defer(() -> parent.client.newHandler(new HttpClientHandler(this, bridge),
 				parent.options.getRemoteAddress(bridge.activeURI),
 				HttpClientOptions.isSecure(bridge.activeURI),
 				bridge))
 		    .retry(bridge)
 		    .cast(HttpClientResponse.class)
 		    .subscribe(subscriber);
+	}
+
+	static final class HttpClientHandler
+			implements BiFunction<NettyInbound, NettyOutbound, Publisher<Void>> {
+
+		final MonoHttpClientResponse parent;
+		final ReconnectableBridge    bridge;
+
+		HttpClientHandler(MonoHttpClientResponse parent, ReconnectableBridge bridge) {
+			this.bridge = bridge;
+			this.parent = parent;
+		}
+
+		@Override
+		public Publisher<Void> apply(NettyInbound in, NettyOutbound out) {
+			try {
+				URI uri = bridge.activeURI;
+				HttpClientOperations ch = (HttpClientOperations) in;
+				ch.getNettyRequest()
+				  .setUri(uri.getPath() + (uri.getQuery() == null ? "" :
+						  "?" + uri.getRawQuery()))
+				  .setMethod(parent.method)
+				  .setProtocolVersion(HttpVersion.HTTP_1_1)
+				  .headers()
+				  .add(HttpHeaderNames.HOST, uri.getHost())
+				  .add(HttpHeaderNames.ACCEPT, ALL);
+
+				if (parent.method == HttpMethod.GET || parent.method == HttpMethod.HEAD) {
+					ch.disableChunkedTransfer();
+				}
+
+				if (parent.handler != null) {
+					return parent.handler.apply(ch);
+				}
+				else {
+					ch.disableChunkedTransfer();
+					return ch.sendHeaders();
+				}
+			}
+			catch (Throwable t) {
+				return Mono.error(t);
+			}
+		}
+
+		@Override
+		public String toString() {
+			return "HttpClientHandler{" + "startURI=" + bridge.activeURI + ", method=" + parent.method + ", handler=" + parent.handler + '}';
+		}
 	}
 
 	static final class ReconnectableBridge
@@ -157,10 +179,6 @@ final class MonoHttpClientResponse extends Mono<HttpClientResponse> {
 		}
 	}
 
-	@Override
-	public String toString() {
-		return "HttpClientHandler{" + "startURI=" + startURI + ", method=" + method +
-				", handler=" + handler + '}';
-	}
+
 }
 
