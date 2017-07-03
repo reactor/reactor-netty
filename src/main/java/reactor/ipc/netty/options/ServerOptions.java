81491afe6797092ba403dc780461cf0b144e70fb
Violeta Georgieva <vgeorgieva@pivotal.io>
2017-07-03 13:02:18 +0300

    Extract ServerOptions.Builder

--- src/main/java/reactor/ipc/netty/options/ServerOptions.java
+++ src/main/java/reactor/ipc/netty/options/ServerOptions.java
@@ -38,82 +38,54 @@ import reactor.ipc.netty.resources.LoopResources;
  * Encapsulates configuration options for server connectors.
  *
  * @author Stephane Maldini
+ * @author Violeta Georgieva
  */
 public class ServerOptions extends NettyOptions<ServerBootstrap, ServerOptions> {
 
 	/**
-	 * Create a new server builder
-	 * @return a new server builder
+	 * Creates a builder for {@link ServerOptions ServerOptions}
+	 *
+	 * @param <BUILDER> A ServerOptions.Builder subclass
+	 * @return a new ServerOptions builder
 	 */
-	public static ServerOptions create() {
-		return new ServerOptions();
-	}
-
-	static void defaultServerOptions(ServerBootstrap bootstrap) {
-		bootstrap.option(ChannelOption.SO_REUSEADDR, true)
-		         .option(ChannelOption.SO_BACKLOG, 1000)
-		         .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-		         .childOption(ChannelOption.SO_RCVBUF, 1024 * 1024)
-		         .childOption(ChannelOption.SO_SNDBUF, 1024 * 1024)
-		         .childOption(ChannelOption.AUTO_READ, false)
-		         .childOption(ChannelOption.SO_KEEPALIVE, true)
-		         .childOption(ChannelOption.SO_LINGER, 0)
-		         .childOption(ChannelOption.TCP_NODELAY, true)
-		         .childOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000);
-	}
-
-	SocketAddress localAddress = LOCALHOST_AUTO_PORT;
-
-	/**
-	 * Build a new {@link ServerBootstrap}.
-	 */
-	protected ServerOptions() {
-		this(new ServerBootstrap());
+	public static <BUILDER extends ServerOptions.Builder<BUILDER>> ServerOptions.Builder<BUILDER> builder() {
+		return builder(new ServerBootstrap());
 	}
 
 	/**
-	 * Apply common option via super constructor then apply
-	 * {@link #defaultServerOptions(ServerBootstrap)}
-	 * to the passed bootstrap.
+	 * Creates a builder for {@link ServerOptions ServerOptions}
 	 *
 	 * @param serverBootstrap the server bootstrap reference to use
+	 * @param <BUILDER> A ServerOptions.Builder subclass
+	 * @return a new ServerOptions builder
 	 */
-	protected ServerOptions(ServerBootstrap serverBootstrap) {
-		super(serverBootstrap);
-		defaultServerOptions(serverBootstrap);
+	public static <BUILDER extends ServerOptions.Builder<BUILDER>> ServerOptions.Builder<BUILDER> builder(ServerBootstrap serverBootstrap) {
+		return new ServerOptions.Builder<>(serverBootstrap);
 	}
 
-	/**
-	 * Deep-copy all references from the passed options into this new
-	 * {@link ServerOptions} instance.
-	 *
-	 * @param options the source options to duplicate
-	 */
-	protected ServerOptions(ServerOptions options) {
-		super(options);
-		this.localAddress = options.localAddress;
-	}
+	private final SocketAddress localAddress;
 
 	/**
-	 * Attribute default attribute to the future {@link Channel} connection. They will
-	 * be available via {@link reactor.ipc.netty.NettyInbound#attr(AttributeKey)}.
-	 *
-	 * @param key the attribute key
-	 * @param value the attribute value
-	 * @param <T> the attribute type
-	 * @return this builder
-	 *
-	 * @see ServerBootstrap#childAttr(AttributeKey, Object)
+	 * Build a new {@link ServerOptions}.
 	 */
-	@Override
-	public <T> ServerOptions attr(AttributeKey<T> key, T value) {
-		bootstrapTemplate.childAttr(key, value);
-		return this;
+	protected ServerOptions(ServerOptions.Builder<?> builder) {
+		super(builder);
+		if (Objects.isNull(builder.listenAddress)) {
+			if (Objects.isNull(builder.host)) {
+				this.localAddress = new InetSocketAddress(builder.port);
+			}
+			else {
+				this.localAddress = new InetSocketAddress(builder.host, builder.port);
+			}
+		}
+		else {
+			this.localAddress = builder.listenAddress;
+		}
 	}
 
 	@Override
 	public ServerOptions duplicate() {
-		return new ServerOptions(this);
+		return builder().from(this).build();
 	}
 
 	@Override
@@ -128,145 +100,11 @@ public class ServerOptions extends NettyOptions<ServerBootstrap, ServerOptions> 
 		return localAddress;
 	}
 
-	/**
-	 * The port on which this server should listen, assuming it should bind to all available addresses.
-	 *
-	 * @param port The port to listen on.
-	 * @return {@literal this}
-	 */
-	public ServerOptions listen(int port) {
-		return listen(new InetSocketAddress(port));
-	}
-
-	/**
-	 * The host on which this server should listen, port will be resolved on bind.
-	 *
-	 * @param host The host to bind to.
-	 *
-	 * @return {@literal this}
-	 */
-	public ServerOptions listen(String host) {
-		if (null == host) {
-			host = "localhost";
-		}
-		return listen(new InetSocketAddress(host, 0));
-	}
-
-	/**
-	 * The host and port on which this server should listen.
-	 *
-	 * @param host The host to bind to.
-	 * @param port The port to listen on.
-	 * @return {@literal this}
-	 */
-	public ServerOptions listen(String host, int port) {
-		if (null == host) {
-			host = "localhost";
-		}
-		return listen(new InetSocketAddress(host, port));
-	}
-
-	/**
-	 * The {@link InetSocketAddress} on which this server should listen.
-	 *
-	 * @param listenAddress the listen address
-	 * @return {@literal this}
-	 */
-	public ServerOptions listen(InetSocketAddress listenAddress) {
-		Objects.requireNonNull(listenAddress, "listenAddress");
-		this.localAddress = listenAddress;
-		return this;
-	}
-
-	/**
-	 * Set a {@link ChannelOption} value for low level connection settings like
-	 * SO_TIMEOUT or SO_KEEPALIVE. This will apply to each new channel from remote
-	 * peer.
-	 *
-	 * @param key the option key
-	 * @param <T> the option type
-	 *
-	 * @return {@code this}
-	 * @see ServerBootstrap#childOption(ChannelOption, Object)
-	 */
-	@Override
-	public <T> ServerOptions option(ChannelOption<T> key, T value) {
-		bootstrapTemplate.childOption(key, value);
-		return this;
-	}
-
-	/**
-	 * Attribute default attribute to the future {@link Channel} connection. They will
-	 * be available via {@link reactor.ipc.netty.NettyInbound#attr(AttributeKey)}.
-	 *
-	 * @param key the attribute key
-	 * @param value the attribute value
-	 * @param <T> the attribute type
-	 * @return this builder
-	 *
-	 * @see ServerBootstrap#attr(AttributeKey, Object)
-	 */
-	public <T> ServerOptions selectorAttr(AttributeKey<T> key, T value) {
-		bootstrapTemplate.childAttr(key, value);
-		return this;
-	}
-
-	/**
-	 * Set a {@link ChannelOption} value for low level selector channel settings like
-	 * SO_TIMEOUT or SO_KEEPALIVE. This will apply to each new channel from remote
-	 * peer.
-	 *
-	 * @param key the option key
-	 * @param <T> the option type
-	 *
-	 * @return {@code this}
-	 *
-	 * @see ServerBootstrap#childOption(ChannelOption, Object)
-	 */
-	public <T> ServerOptions selectorOption(ChannelOption<T> key, T value) {
-		bootstrapTemplate.childOption(key, value);
-		return this;
-	}
-
-	/**
-	 * Enable SSL service with a self-signed certificate
-	 *
-	 * @return {@code this}
-	 */
-	public ServerOptions sslSelfSigned() {
-		return sslSelfSigned(c -> {
-		});
-	}
-	
-	/**
-	 * Enable SSL service with a self-signed certificate and allows extra
-	 * parameterization of the self signed {@link SslContextBuilder}. The builder is
-	 * then used to invoke {@link #sslContext(SslContext)}.
-	 *
-	 * @param configurator the builder callback to setup the self-signed {@link SslContextBuilder}
-	 *
-	 * @return {@code this}
-	 */
-	public ServerOptions sslSelfSigned(Consumer<? super SslContextBuilder> configurator) {
-		Objects.requireNonNull(configurator, "configurator");
-		SelfSignedCertificate ssc;
-		try {
-			ssc = new SelfSignedCertificate();
-			SslContextBuilder builder =
-					SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey());
-			configurator.accept(builder);
-			return sslContext(builder.build());
-		}
-		catch (Exception sslException) {
-			throw Exceptions.bubble(sslException);
-		}
-	}
-
 	final void groupAndChannel(ServerBootstrap bootstrap) {
 		LoopResources loops =
-				Objects.requireNonNull(this.loopResources, "loopResources");
+				Objects.requireNonNull(getLoopResources(), "loopResources");
 
-		boolean useNative = preferNative && !(sslContext instanceof JdkSslContext);
+		boolean useNative = preferNative() && !(sslContext() instanceof JdkSslContext);
 		final EventLoopGroup selectorGroup = loops.onServerSelect(useNative);
 		final EventLoopGroup elg = loops.onServer(useNative);
 
@@ -289,5 +127,186 @@ public class ServerOptions extends NettyOptions<ServerBootstrap, ServerOptions> 
 		return "ServerOptions{" + asDetailedString() + "}";
 	}
 
-	final static InetSocketAddress LOCALHOST_AUTO_PORT = new InetSocketAddress(0);
+	public static class Builder<BUILDER extends Builder<BUILDER>>
+			extends NettyOptions.Builder<ServerBootstrap, ServerOptions, BUILDER> {
+		private String host;
+		private int port;
+		private SocketAddress listenAddress;
+
+		/**
+		 * Apply common option via super class then apply
+		 * {@link #defaultServerOptions(ServerBootstrap)}
+		 * to the passed bootstrap.
+		 *
+		 * @param serverBootstrap the server bootstrap reference to use
+		 */
+		protected Builder(ServerBootstrap serverBootstrap) {
+			super(serverBootstrap);
+			defaultServerOptions(serverBootstrap);
+		}
+
+		private final void defaultServerOptions(ServerBootstrap bootstrap) {
+			bootstrap.option(ChannelOption.SO_REUSEADDR, true)
+			         .option(ChannelOption.SO_BACKLOG, 1000)
+			         .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+			         .childOption(ChannelOption.SO_RCVBUF, 1024 * 1024)
+			         .childOption(ChannelOption.SO_SNDBUF, 1024 * 1024)
+			         .childOption(ChannelOption.AUTO_READ, false)
+			         .childOption(ChannelOption.SO_KEEPALIVE, true)
+			         .childOption(ChannelOption.SO_LINGER, 0)
+			         .childOption(ChannelOption.TCP_NODELAY, true)
+			         .childOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000);
+		}
+
+		/**
+		 * Attribute default attribute to the future {@link Channel} connection. They will
+		 * be available via {@link reactor.ipc.netty.NettyInbound#attr(AttributeKey)}.
+		 *
+		 * @param key the attribute key
+		 * @param value the attribute value
+		 * @param <T> the attribute type
+		 * @return {@code this}
+		 * @see ServerBootstrap#childAttr(AttributeKey, Object)
+		 */
+		@Override
+		public final <T> BUILDER attr(AttributeKey<T> key, T value) {
+			this.bootstrapTemplate.childAttr(key, value);
+			return get();
+		}
+
+		/**
+		 * Set a {@link ChannelOption} value for low level connection settings like
+		 * SO_TIMEOUT or SO_KEEPALIVE. This will apply to each new channel from remote
+		 * peer.
+		 *
+		 * @param key the option key
+		 * @param <T> the option type
+		 * @return {@code this}
+		 * @see ServerBootstrap#childOption(ChannelOption, Object)
+		 */
+		@Override
+		public final <T> BUILDER option(ChannelOption<T> key, T value) {
+			this.bootstrapTemplate.childOption(key, value);
+			return get();
+		}
+
+		/**
+		 * Attribute default attribute to the future {@link Channel} connection. They will
+		 * be available via {@link reactor.ipc.netty.NettyInbound#attr(AttributeKey)}.
+		 *
+		 * @param key the attribute key
+		 * @param value the attribute value
+		 * @param <T> the attribute type
+		 * @return this builder
+		 * @see ServerBootstrap#childAttr(AttributeKey, Object)
+		 */
+		public final <T> BUILDER selectorAttr(AttributeKey<T> key, T value) {
+			attr(key, value);
+			return get();
+		}
+
+		/**
+		 * Set a {@link ChannelOption} value for low level selector channel settings like
+		 * SO_TIMEOUT or SO_KEEPALIVE. This will apply to each new channel from remote
+		 * peer.
+		 *
+		 * @param key the option key
+		 * @param value the option value
+		 * @param <T> the option type
+		 * @return {@code this}
+		 * @see ServerBootstrap#childOption(ChannelOption, Object)
+		 */
+		public final <T> BUILDER selectorOption(ChannelOption<T> key, T value) {
+			option(key, value);
+			return get();
+		}
+
+		/**
+		 * The host on which this server should listen.
+		 *
+		 * @param host The host to bind to.
+		 * @return {@code this}
+		 */
+		public final BUILDER host(String host) {
+			if (Objects.isNull(host)) {
+				this.host = "localhost";
+			}
+			else {
+				this.host = host;
+			}
+			return get();
+		}
+
+		/**
+		 * The port on which this server should listen, assuming it should bind to all available addresses.
+		 *
+		 * @param port The port to listen on.
+		 * @return {@code this}
+		 */
+		public final BUILDER port(int port) {
+			this.port = Objects.requireNonNull(port, "port");
+			return get();
+		}
+
+		/**
+		 * The {@link InetSocketAddress} on which this server should listen.
+		 *
+		 * @param listenAddress the listen address
+		 * @return {@code this}
+		 */
+		public final BUILDER listenAddress(SocketAddress listenAddress) {
+			this.listenAddress = Objects.requireNonNull(listenAddress, "listenAddress");
+			return get();
+		}
+
+		/**
+		 * Enable SSL service with a self-signed certificate
+		 *
+		 * @return {@code this}
+		 */
+		public final BUILDER sslSelfSigned() {
+			return sslSelfSigned(c -> {});
+		}
+
+		/**
+		 * Enable SSL service with a self-signed certificate and allows extra
+		 * parameterization of the self signed {@link SslContextBuilder}. The builder is
+		 * then used to invoke {@link #sslContext(SslContext)}.
+		 *
+		 * @param configurator the builder callback to setup the self-signed {@link SslContextBuilder}
+		 * @return {@code this}
+		 */
+		public final BUILDER sslSelfSigned(Consumer<? super SslContextBuilder> configurator) {
+			Objects.requireNonNull(configurator, "configurator");
+			SelfSignedCertificate ssc;
+			try {
+				ssc = new SelfSignedCertificate();
+				SslContextBuilder builder =
+						SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey());
+				configurator.accept(builder);
+				sslContext(builder.build());
+				return get();
+			}
+			catch (Exception sslException) {
+				throw Exceptions.bubble(sslException);
+			}
+		}
+
+		@Override
+		public final BUILDER from(ServerOptions options) {
+			super.from(options);
+			this.listenAddress = options.localAddress;
+			return get();
+		}
+
+		@Override
+		@SuppressWarnings("unchecked")
+		public BUILDER get() {
+			return (BUILDER) this;
+		}
+
+		public ServerOptions build() {
+			return new ServerOptions(this);
+		}
+	}
 }
