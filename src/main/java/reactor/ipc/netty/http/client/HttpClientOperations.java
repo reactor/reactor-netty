4a79b0397afccbb347a2854c8dddd1c2a7b4900a
Violeta Georgieva <vgeorgieva@pivotal.io>
2017-09-07 23:25:05 +0300

    Throw IOException when channel inactive received

--- src/main/java/reactor/ipc/netty/http/client/HttpClientOperations.java
+++ src/main/java/reactor/ipc/netty/http/client/HttpClientOperations.java
@@ -16,6 +16,7 @@
 
 package reactor.ipc.netty.http.client;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -258,6 +259,15 @@ class HttpClientOperations extends HttpOperations<HttpClientResponse, HttpClient
 	@Override
 	protected void onInboundCancel() {
 		channel().close();
+	}
+
+	@Override
+	protected void onInboundComplete() {
+		if (responseState == null) {
+			parentContext().fireContextError(new IOException("Connection closed prematurely"));
+			return;
+		}
+		super.onInboundComplete();
 	}
 
 	@Override
