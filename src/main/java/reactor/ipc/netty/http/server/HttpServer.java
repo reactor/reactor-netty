7b9bac570daf583f870ea487934b07382456ef1c
Simon Baslé <sbasle@pivotal.io>
2018-01-15 20:45:44 +0200

    fix #150 Add a configuration API for HttpCodec
    This commits allows to configure Netty's HttpServerCodec 5 options for
    request decoding, through an HttpServer API that consumes a small simple
    builder which holds the parameters. Parameters are then injected as
    Channel attributes.
    
    This would notably allow the codec to decode requests on very long URIs.

--- src/main/java/reactor/ipc/netty/http/server/HttpServer.java
+++ src/main/java/reactor/ipc/netty/http/server/HttpServer.java
@@ -236,6 +236,18 @@ public abstract class HttpServer {
 	}
 
 	/**
+	 * Configure the {@link io.netty.handler.codec.http.HttpServerCodec}'s request decoding options.
+	 *
+	 * @param requestDecoderOptions a function to mutate the provided Http request decoder options
+	 * @return a new {@link HttpServer}
+	 */
+	public final HttpServer httpRequestDecoder(Function<HttpRequestDecoderConfiguration, HttpRequestDecoderConfiguration> requestDecoderOptions) {
+		return tcpConfiguration(
+				requestDecoderOptions.apply(new HttpRequestDecoderConfiguration())
+				                     .build());
+	}
+
+	/**
 	 * Disable support for the {@code "Forwarded"} and {@code "X-Forwarded-*"}
 	 * HTTP request headers.
 	 *
