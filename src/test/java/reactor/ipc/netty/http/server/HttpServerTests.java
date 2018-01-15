a0f88409a90df1a1c1c41dd0fc4e72ad3e576d64
Stephane Maldini <smaldini@pivotal.io>
2018-01-15 03:03:12 -0800

    add testt timeout

--- src/test/java/reactor/ipc/netty/http/server/HttpServerTests.java
+++ src/test/java/reactor/ipc/netty/http/server/HttpServerTests.java
@@ -104,7 +104,6 @@ public class HttpServerTests {
 	}
 
 	@Test
-	@Ignore
 	public void defaultHttpPortWithAddress() {
 		DisposableServer blockingFacade = HttpServer.create()
 		                                      .tcpConfiguration(tcpServer -> tcpServer.host("localhost"))
