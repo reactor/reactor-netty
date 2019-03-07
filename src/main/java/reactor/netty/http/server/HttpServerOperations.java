2238e101ea7a21e51cba094c75db1563dcd44e2c
Stephane Maldini <smaldini@pivotal.io>
2019-03-07 10:49:20 -0800

    tweak websocket subscriber error forwarding

--- src/main/java/reactor/netty/http/server/HttpServerOperations.java
+++ src/main/java/reactor/netty/http/server/HttpServerOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019 Pivotal Software Inc, All Rights Reserved.
+ * Copyright (c) 2011-Present Pivotal Software Inc, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -601,7 +601,7 @@ class HttpServerOperations extends HttpOperations<HttpServerRequest, HttpServerR
 
 		@Override
 		public void onError(Throwable t) {
-			ops.onOutboundError(t);
+			ops.onError(t);
 		}
 
 		@Override
