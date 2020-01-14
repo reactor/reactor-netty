24675591cab7c53760bc08dda8a1fa427981e325
Violeta Georgieva <vgeorgieva@pivotal.io>
2020-01-14 11:55:32 +0200

    Polish

--- src/main/java/reactor/netty/http/HttpOperations.java
+++ src/main/java/reactor/netty/http/HttpOperations.java
@@ -316,13 +316,13 @@ public abstract class HttpOperations<INBOUND extends NettyInbound, OUTBOUND exte
 			path = URI.create(uri).getPath();
 		}
 		if (!path.isEmpty()) {
-			if(path.charAt(0) == '/'){
+			if (path.charAt(0) == '/') {
 				path = path.substring(1);
-				if(path.length() <= 1){
+				if (path.length() <= 1) {
 					return path;
 				}
 			}
-			if(path.charAt(path.length() - 1) == '/'){
+			if (path.charAt(path.length() - 1) == '/') {
 				return path.substring(0, path.length() - 1);
 			}
 		}
