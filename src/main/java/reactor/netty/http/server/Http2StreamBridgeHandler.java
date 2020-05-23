d037bf059af38eeca60ea008cbcedb683165c46e
Violeta Georgieva <violetag@vmware.com>
2020-05-23 15:40:56 +0300

    Revert "When HttpServer in H2 mode, release FullHttpRequest in case of failure"
    This reverts commit b34919f1df1eb92aa9f815472685edc0307485da.

--- src/main/java/reactor/netty/http/server/Http2StreamBridgeHandler.java
+++ src/main/java/reactor/netty/http/server/Http2StreamBridgeHandler.java
@@ -24,7 +24,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.DefaultHttpContent;
-import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.cookie.ServerCookieDecoder;
 import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
@@ -88,9 +87,6 @@ final class Http2StreamBridgeHandler extends ChannelDuplexHandler {
 						cookieDecoder);
 			}
 			catch (RuntimeException e) {
-				if (request instanceof FullHttpRequest) {
-					((FullHttpRequest) request).release();
-				}
 				HttpServerOperations.sendDecodingFailures(ctx, e, msg);
 				return;
 			}
