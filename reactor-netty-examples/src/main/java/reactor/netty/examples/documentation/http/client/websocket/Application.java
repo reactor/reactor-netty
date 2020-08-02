df6b7ce6438df50ea5ce2841c4ddec4ff598cae4
Violeta Georgieva <violetag@vmware.com>
2020-08-02 16:33:21 +0300

    Move the HTTP client examples from the documentation to the examples submodule

---
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/http/client/websocket/Application.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2011-Present VMware, Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.netty.examples.documentation.http.client.websocket;
+
+import io.netty.buffer.Unpooled;
+import io.netty.util.CharsetUtil;
+import reactor.core.publisher.Flux;
+import reactor.netty.http.client.HttpClient;
+
+public class Application {
+
+	public static void main(String[] args) {
+		HttpClient client = HttpClient.create();
+
+		client.websocket()
+		      .uri("wss://echo.websocket.org")
+		      .handle((inbound, outbound) -> {
+		          inbound.receive()
+		                 .asString()
+		                 .take(1)
+		                 .subscribe(System.out::println);
+
+		          final byte[] msgBytes = "hello".getBytes(CharsetUtil.ISO_8859_1);
+		          return outbound.send(Flux.just(Unpooled.wrappedBuffer(msgBytes), Unpooled.wrappedBuffer(msgBytes)))
+		                         .neverComplete();
+		      })
+		      .blockLast();
+	}
+}
