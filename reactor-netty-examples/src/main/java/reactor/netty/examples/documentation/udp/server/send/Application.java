b139f53224e58c537a4b445d44a28c5cb672a389
Violeta Georgieva <violetag@vmware.com>
2020-08-02 18:42:09 +0300

    Move the UDP server examples from the documentation to the examples submodule

---
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/udp/server/send/Application.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2011-Present VMware, Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.netty.examples.documentation.udp.server.send;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.util.CharsetUtil;
+import reactor.core.publisher.Mono;
+import reactor.netty.Connection;
+import reactor.netty.udp.UdpServer;
+
+import java.time.Duration;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Connection server =
+				UdpServer.create()
+				         .handle((in, out) ->
+				             out.sendObject(
+				                 in.receiveObject()
+				                   .map(o -> {
+				                       if (o instanceof DatagramPacket) {
+				                           DatagramPacket p = (DatagramPacket) o;
+				                           ByteBuf buf = Unpooled.copiedBuffer("hello", CharsetUtil.UTF_8);
+				                           return new DatagramPacket(buf, p.sender()); //<1>
+				                       }
+				                       else {
+				                           return Mono.error(new Exception("Unexpected type of the message: " + o));
+				                       }
+				                   })))
+				         .bindNow(Duration.ofSeconds(30));
+
+		server.onDispose()
+		      .block();
+	}
+}
