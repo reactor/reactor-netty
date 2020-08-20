e036f396240679f8d25501dd6ac5b49708f7f6c4
Violeta Georgieva <violetag@vmware.com>
2020-08-20 23:17:08 +0300

    Add HTTP/2 client examples to the reference documentation

---
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/http/client/http2/H2Application.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2011-Present VMware, Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.netty.examples.documentation.http.client.http2;
+
+import io.netty.handler.codec.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
+import reactor.util.function.Tuple2;
+
+public class H2Application {
+
+	public static void main(String[] args) {
+		HttpClient client =
+				HttpClient.create()
+				          .protocol(HttpProtocol.H2) //<1>
+				          .secure();                 //<2>
+
+		Tuple2<String, HttpHeaders> response =
+				client.get()
+				      .uri("https://example.com/")
+				      .responseSingle((res, bytes) -> bytes.asString()
+				                                           .zipWith(Mono.just(res.responseHeaders())))
+				      .block();
+
+		System.out.println("Used stream ID: " + response.getT2().get("x-http2-stream-id"));
+		System.out.println("Response: " + response.getT1());
+	}
+}
