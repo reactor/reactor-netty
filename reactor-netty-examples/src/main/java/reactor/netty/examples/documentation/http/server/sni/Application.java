5be0b6e727d72ecf8f1d6d4664aadd4736d49a00
Violeta Georgieva <violetag@vmware.com>
2020-08-20 20:16:24 +0300

    Add SNI examples to the reference documentation

---
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/http/server/sni/Application.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2011-Present VMware, Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.netty.examples.documentation.http.server.sni;
+
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+
+import java.io.File;
+
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+		File defaultCert = new File("default_certificate.crt");
+		File defaultKey = new File("default_private.key");
+
+		File testDomainCert = new File("default_certificate.crt");
+		File testDomainKey = new File("default_private.key");
+
+		SslContext defaultSslContext = SslContextBuilder.forServer(defaultCert, defaultKey).build();
+		SslContext testDomainSslContext = SslContextBuilder.forServer(testDomainCert, testDomainKey).build();
+
+		DisposableServer server =
+				HttpServer.create()
+				          .secure(spec -> spec.sslContext(defaultSslContext)
+				                              .addSniMapping("*.test.com",
+				                                      testDomainSpec -> testDomainSpec.sslContext(testDomainSslContext)))
+				          .bindNow();
+
+		server.onDispose()
+		      .block();
+	}
+}
