40ca04fd4848f06f19f9e7d3a03a56bc1dc9b0bc
Violeta Georgieva <violetag@vmware.com>
2020-09-10 11:15:10 +0300

    Update HTTP/2 default ciphers
    Related to
    https://github.com/netty/netty/pull/10418
    https://github.com/netty/netty/pull/10480

--- reactor-netty-core/src/main/java/reactor/netty/tcp/SslProvider.java
+++ reactor-netty-core/src/main/java/reactor/netty/tcp/SslProvider.java
@@ -255,7 +255,7 @@ public final class SslProvider {
 		/**
 		 * {@link io.netty.handler.ssl.SslProvider} will be set depending on
 		 * <code>OpenSsl.isAlpnSupported()</code>,
-		 * {@link #HTTP2_TLS12_CIPHERS},
+		 * {@link #HTTP2_CIPHERS},
 		 * ALPN support,
 		 * HTTP/1.1 and HTTP/2 support
 		 */
@@ -400,7 +400,7 @@ public final class SslProvider {
 				                     io.netty.handler.ssl.SslProvider.isAlpnSupported(io.netty.handler.ssl.SslProvider.OPENSSL) ?
 				                             io.netty.handler.ssl.SslProvider.OPENSSL :
 				                             io.netty.handler.ssl.SslProvider.JDK)
-				                 .ciphers(HTTP2_TLS12_CIPHERS, SupportedCipherSuiteFilter.INSTANCE)
+				                 .ciphers(HTTP2_CIPHERS, SupportedCipherSuiteFilter.INSTANCE)
 				                 .applicationProtocolConfig(new ApplicationProtocolConfig(
 				                     ApplicationProtocolConfig.Protocol.ALPN,
 				                     ApplicationProtocolConfig.SelectorFailureBehavior.NO_ADVERTISE,
@@ -772,8 +772,16 @@ public final class SslProvider {
 	 * <a href="https://tools.ietf.org/html/rfc7540#appendix-A">HTTP/2 RFC</a>.
 	 * Copied from io.netty.handler.codec.http2.Http2SecurityUtil
 	 */
-	static final List<String> HTTP2_TLS12_CIPHERS =
+	static final List<String> HTTP2_CIPHERS =
 			Collections.unmodifiableList(Arrays.asList(
+					/* openssl = ECDHE-ECDSA-AES128-GCM-SHA256 */
+					"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+
+					/* REQUIRED BY HTTP/2 SPEC */
+					/* openssl = ECDHE-RSA-AES128-GCM-SHA256 */
+					"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+					/* REQUIRED BY HTTP/2 SPEC */
+
 					/* openssl = ECDHE-ECDSA-AES256-GCM-SHA384 */
 					"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
 					/* openssl = ECDHE-RSA-AES256-GCM-SHA384 */
@@ -782,14 +790,9 @@ public final class SslProvider {
 					"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
 					/* openssl = ECDHE-RSA-CHACHA20-POLY1305 */
 					"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
-					/* openssl = ECDHE-ECDSA-AES128-GCM-SHA256 */
-					"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
 
-					/* REQUIRED BY HTTP/2 SPEC */
-					/* openssl = ECDHE-RSA-AES128-GCM-SHA256 */
-					"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
-					/* REQUIRED BY HTTP/2 SPEC */));
+					/* TLS 1.3 ciphers */
+					"TLS_AES_128_GCM_SHA256",
+					"TLS_AES_256_GCM_SHA384",
+					"TLS_CHACHA20_POLY1305_SHA256"));
 }
-
-
-
