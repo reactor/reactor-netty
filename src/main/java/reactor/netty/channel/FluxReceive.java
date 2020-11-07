reactor/reactor-netty

commit 5e4bc03c76967b5b5aa8350a66f315b3e1032e4a

Enhances internals taking into account no concurrency in eventLoop (#1185)
Signed-off-by: Oleh Dokuka <shadowgun@i.ua>

src/main/java/reactor/netty/channel/FluxReceive.java
@@ -17,6 +17,7 @@
 package reactor.netty.channel;
 
 import java.nio.channels.ClosedChannelException;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
@@ -34,7 +35,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Operators;
 import reactor.util.Logger;
 import reactor.util.Loggers;
-import reactor.util.concurrent.Queues;
 
 import static reactor.netty.ReactorNetty.format;
 
@@ -60,10 +60,19 @@ final class FluxReceive extends Flux<Object> implements Subscription, Disposable
 	Throwable inboundError;
 
 	volatile Disposable receiverCancel;
-	volatile int wip;
 
-	final static AtomicIntegerFieldUpdater<FluxReceive> WIP = AtomicIntegerFieldUpdater.newUpdater
-			(FluxReceive.class, "wip");
+	volatile int once;
+	static final AtomicIntegerFieldUpdater<FluxReceive> ONCE =
+		AtomicIntegerFieldUpdater.newUpdater(FluxReceive.class, "once");
+
+	// Please note, in this specific case WIP is non-volatile since all operation that
+	// involves work-in-progress pattern is within Netty Event-Loops which guarantees
+	// serial, thread-safe behaviour.
+	// However, we need that flag in order to preserve work-in-progress guarding that
+	// prevents stack overflow in case of onNext -> request -> onNext cycling on the
+	// same stack
+	int wip;
+
 
 	FluxReceive(ChannelOperations<?, ?> parent) {
 
@@ -131,11 +140,42 @@ final class FluxReceive extends Flux<Object> implements Subscription, Disposable
 
 	@Override
 	public void subscribe(CoreSubscriber<? super Object> s) {
-		if (eventLoop.inEventLoop()){
-			startReceiver(s);
+		if (once == 0 && ONCE.compareAndSet(this, 0, 1)) {
+			if (log.isDebugEnabled()) {
+				log.debug(format(channel, "Subscribing inbound receiver [pending: {}, cancelled:{}, " +
+								"inboundDone: {}]"),
+						getPending(),
+						isCancelled(),
+						inboundDone);
+			}
+			if (inboundDone && getPending() == 0) {
+				if (inboundError != null) {
+					Operators.error(s, inboundError);
+					return;
+				}
+
+				Operators.complete(s);
+				return;
+			}
+
+			receiver = s;
+
+			s.onSubscribe(this);
 		}
 		else {
-			eventLoop.execute(() -> startReceiver(s));
+			if (inboundDone && getPending() == 0) {
+				if (inboundError != null) {
+					Operators.error(s, inboundError);
+					return;
+				}
+
+				Operators.complete(s);
+			}
+			else {
+				Operators.error(s,
+						new IllegalStateException(
+								"Only one connection receive subscriber allowed."));
+			}
 		}
 	}
 
@@ -164,7 +204,8 @@ final class FluxReceive extends Flux<Object> implements Subscription, Disposable
 	}
 
 	final void drainReceiver() {
-		if(WIP.getAndIncrement(this) != 0){
+		// general protect against stackoverflow onNext -> request -> onNext
+		if (wip++ != 0) {
 			return;
 		}
 		int missed = 1;
@@ -185,7 +226,7 @@ final class FluxReceive extends Flux<Object> implements Subscription, Disposable
 					}
 					return;
 				}
-				missed = WIP.addAndGet(this, -missed);
+				missed = (wip -= missed);
 				if(missed == 0){
 					break;
 				}
@@ -250,7 +291,7 @@ final class FluxReceive extends Flux<Object> implements Subscription, Disposable
 					channel.config()
 					       .setAutoRead(true);
 				}
-				missed = WIP.addAndGet(this, -missed);
+				missed = (wip -= missed);
 				if(missed == 0){
 					break;
 				}
@@ -269,40 +310,10 @@ final class FluxReceive extends Flux<Object> implements Subscription, Disposable
 				       .setAutoRead(false);
 			}
 
-			missed = WIP.addAndGet(this, -missed);
+			missed = (wip -= missed);
 			if(missed == 0){
 				break;
 			}
-		}
-	}
-
-	final void startReceiver(CoreSubscriber<? super Object> s) {
-		if (receiver == null) {
-			if (log.isDebugEnabled()) {
-				log.debug(format(channel, "Subscribing inbound receiver [pending: {}, cancelled:{}, " +
-								"inboundDone: {}]"),
-						getPending(),
-						isCancelled(),
-						inboundDone);
-			}
-			if (inboundDone && getPending() == 0) {
-				if (inboundError != null) {
-					Operators.error(s, inboundError);
-					return;
-				}
-
-				Operators.complete(s);
-				return;
-			}
-
-			receiver = s;
-
-			s.onSubscribe(this);
-		}
-		else {
-			Operators.error(s,
-					new IllegalStateException(
-							"Only one connection receive subscriber allowed."));
 		}
 	}
 
@@ -336,8 +347,10 @@ final class FluxReceive extends Flux<Object> implements Subscription, Disposable
 		else {
 			Queue<Object> q = receiverQueue;
 			if (q == null) {
-				q = Queues.unbounded()
-				          .get();
+				// please note, in that case we are using non-thread safe, simple
+				// ArrayDeque since all modifications on this queue happens withing
+				// Netty Event Loop
+				q = new ArrayDeque<>();
 				receiverQueue = q;
 			}
 			if (log.isDebugEnabled()){

commit 1a4651dfe51da73d21d9b21274de8562ef987d92

Guarantee there is no race between subscribe and other operations invoked in an event loop
This is a quick fix for the regression introduced with #1185

Related to #1370 and #1350

src/main/java/reactor/netty/channel/FluxReceive.java
@@ -140,6 +140,15 @@ final class FluxReceive extends Flux<Object> implements Subscription, Disposable
 
 	@Override
 	public void subscribe(CoreSubscriber<? super Object> s) {
+		if (eventLoop.inEventLoop()){
+			startReceiver(s);
+		}
+		else {
+			eventLoop.execute(() -> startReceiver(s));
+		}
+	}
+
+	final void startReceiver(CoreSubscriber<? super Object> s) {
 		if (once == 0 && ONCE.compareAndSet(this, 0, 1)) {
 			if (log.isDebugEnabled()) {
 				log.debug(format(channel, "{}: subscribing inbound receiver"), this);