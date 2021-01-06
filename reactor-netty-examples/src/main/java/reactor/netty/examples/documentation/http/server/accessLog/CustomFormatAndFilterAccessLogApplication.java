83af5d06c78d3e6faeacf01b2919f682a70888ee
Audrey Neveu <aneveu@vmware.com>
2021-01-06 16:53:49 +0100

    add samples and documentation for accessLog programatic configuration

---
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/http/server/accessLog/CustomFormatAndFilterAccessLogApplication.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2011-Present VMware, Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.netty.examples.documentation.http.server.accessLog;
+
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.http.server.logging.AccessLog;
+import reactor.netty.http.server.logging.AccessLogFactory;
+
+public class CustomFormatAndFilterAccessLogApplication {
+
+	public static void main(String[] args) {
+		DisposableServer server =
+				HttpServer.create()
+				          .accessLog(true, AccessLogFactory.createFilter(p -> !String.valueOf(p.uri()).startsWith("/health/"), //<1>
+						          x -> AccessLog.create("method={}, uri={}", x.method(), x.uri()))) //<2>
+				          .bindNow();
+
+		server.onDispose()
+		      .block();
+	}
+}
