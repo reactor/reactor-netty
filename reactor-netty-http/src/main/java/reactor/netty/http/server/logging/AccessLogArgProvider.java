df15c3a43ae1934d49b65c18025b48ee313e74cd
Violeta Georgieva <violetag@vmware.com>
2021-04-05 11:47:00 +0300

    Address feedback

--- reactor-netty-http/src/main/java/reactor/netty/http/server/logging/AccessLogArgProvider.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/server/logging/AccessLogArgProvider.java
@@ -135,6 +135,9 @@ public interface AccessLogArgProvider {
 
 	/**
 	 * Returns resolved HTTP cookies.
+	 * <p>
+	 * Warning: Be cautious with cookies information and what kind of sensitive data is written to the logs.
+	 * By default no cookies information is written to the access log.
 	 *
 	 * @return Resolved HTTP cookies
 	 * @since 1.0.6
