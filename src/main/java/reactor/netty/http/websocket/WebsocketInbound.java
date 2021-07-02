fad46f6c172a7aa4e397905f7b453118f1d408ef
Violeta Georgieva <violetag@vmware.com>
2021-07-02 15:40:26 +0300

    Remove extra empty line between license and package

--- src/main/java/reactor/netty/http/websocket/WebsocketInbound.java
+++ src/main/java/reactor/netty/http/websocket/WebsocketInbound.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package reactor.netty.http.websocket;
 
 import io.netty.handler.codec.http.HttpHeaders;
--- src/main/java/reactor/netty/http/websocket/WebsocketSpec.java
+++ src/main/java/reactor/netty/http/websocket/WebsocketSpec.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package reactor.netty.http.websocket;
 
 import javax.annotation.Nullable;
