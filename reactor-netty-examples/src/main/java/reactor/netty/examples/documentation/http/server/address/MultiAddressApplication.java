dbc5f8526d611e938a38e40fc8eb143d0d3d3c57
放浪形骸 <eurekajameschen@gmail.com>
2021-08-09 05:38:19 -0700

    Add docs for `Tcp|HttpServer`s with same configuration but different bind addresses (#1760)
    Related to #1751 
    
    Co-authored-by: Simon Baslé <simon.basle@gmail.com>
    Co-authored-by: Violeta Georgieva <milesg78@gmail.com>

---
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/http/server/address/MultiAddressApplication.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021 VMware, Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.netty.examples.documentation.http.server.address;
+
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+
+public class MultiAddressApplication {
+	public static void main(String[] args) {
+		HttpServer httpServer = HttpServer.create();
+		DisposableServer server1 = httpServer
+				.host("localhost") //<1>
+				.port(8080)        //<2>
+				.bindNow();
+
+		DisposableServer server2 = httpServer
+				.host("0.0.0.0") //<3>
+				.port(8081)      //<4>
+				.bindNow();
+
+		Mono.when(server1.onDispose(), server2.onDispose())
+				.block();
+	}
+}
