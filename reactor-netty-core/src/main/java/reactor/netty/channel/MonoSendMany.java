32ca7e438e9482be9b339c222fa639cb77fdccc5
Oleh Dokuka <5380167+OlegDokuka@users.noreply.github.com>
2021-09-23 15:50:00 +0300

    Update reactor-netty-core/src/main/java/reactor/netty/channel/MonoSendMany.java
    Co-authored-by: Violeta Georgieva <violetag@vmware.com>

--- reactor-netty-core/src/main/java/reactor/netty/channel/MonoSendMany.java
+++ reactor-netty-core/src/main/java/reactor/netty/channel/MonoSendMany.java
@@ -577,14 +577,7 @@ final class MonoSendMany<I, O> extends MonoSend<I, O> implements Scannable {
 			requested--;
 			pending--;
 
-			if (checkTerminated()) {
-				if (sourceMode == SYNC) {
-					if (requested <= REFILL_SIZE) {
-						int u = MAX_SIZE - requested;
-						requested += u;
-						nextRequest += u;
-					}
-				}
+			if (checkTerminated() && sourceMode != SYNC) {
 				trySchedule();
 				return true;
 			}
