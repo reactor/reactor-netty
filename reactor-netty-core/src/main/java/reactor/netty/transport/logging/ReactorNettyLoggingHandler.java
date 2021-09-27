12914a95d47fbc7995708ace6228f084433daffd
Violeta Georgieva <violetag@vmware.com>
2021-09-27 17:08:23 +0300

    Remove 'id:' from the logs when presenting the channel id (#1826)

--- reactor-netty-core/src/main/java/reactor/netty/transport/logging/ReactorNettyLoggingHandler.java
+++ reactor-netty-core/src/main/java/reactor/netty/transport/logging/ReactorNettyLoggingHandler.java
@@ -165,16 +165,16 @@ final class ReactorNettyLoggingHandler extends LoggingHandler {
 		Connection connection = Connection.from(channel);
 		if (connection instanceof ChannelOperationsId) {
 			String channelStr = ((ChannelOperationsId) connection).asLongText();
-			return new StringBuilder(4 + channelStr.length() + 1)
+			return new StringBuilder(1 + channelStr.length() + 1)
 					.append(CHANNEL_ID_PREFIX)
 					.append(channelStr)
 					.append(CHANNEL_ID_SUFFIX)
 					.toString();
 		}
 		else {
-			// Replace "[id: 0x" with "[id:" in order to keep it consistent with ChannelOperationsId#asLongText()
+			// Replace "[id: 0x" with '[' in order to keep it consistent with ChannelOperationsId#asLongText()
 			String channelStr = channel.toString().substring(ORIGINAL_CHANNEL_ID_PREFIX_LENGTH);
-			return new StringBuilder(4 + channelStr.length())
+			return new StringBuilder(1 + channelStr.length())
 					.append(CHANNEL_ID_PREFIX)
 					.append(channelStr)
 					.toString();
@@ -291,6 +291,6 @@ final class ReactorNettyLoggingHandler extends LoggingHandler {
 	}
 
 	static final int ORIGINAL_CHANNEL_ID_PREFIX_LENGTH = "[id: 0x".length();
-	static final String CHANNEL_ID_PREFIX = "[id:";
+	static final char CHANNEL_ID_PREFIX = '[';
 	static final char CHANNEL_ID_SUFFIX = ']';
 }
