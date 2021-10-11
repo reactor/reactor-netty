fcd1160f67873fed150cab0119d0628562942c15
Simon Baslé <sbasle@vmware.com>
2021-10-11 15:36:57 +0200

    Minimum coverage of channel init issue without micrometer
    This commit attempts at covering the channel initialization issue when
    metrics recorders are registered during configuration but Micrometer is
    not on the classpath.
    
    The NoClassDefFoundError itself is not accessible. It is swallowed by
    Netty during channel initialization.
    
    This is not ideal but we can still detect something is off, since the
    HttpClient does throw a StacklessClosedChannelException (tested on tag
    v1.0.11).
    
    See #1847.
    See #1848.

---
+++ reactor-netty-http/src/noMicrometerTest/java/reactor/netty/http/client/HttpClientNoMicrometerTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2011-2021 VMware, Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.netty.http.client;
+
+import java.net.SocketAddress;
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.netty.DisposableServer;
+import reactor.netty.channel.ChannelMetricsRecorder;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.resources.ConnectionPoolMetrics;
+import reactor.netty.resources.ConnectionProvider;
+import reactor.util.Metrics;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+/**
+ * @author Simon Baslé
+ */
+class HttpClientNoMicrometerTest {
+
+	@Test
+	void smokeTestNoMicrometer() {
+		assertThat(Metrics.isInstrumentationAvailable()).as("isInstrumentationAvailable").isFalse();
+	}
+
+	@Test
+	void clientCreatedWithMetricsDoesntLoadGauge() {
+		//note that this test cannot really access and assert the NoClassDefFoundException, which is forcefully logged by Netty
+		//But when that error occurs, the channel cannot be initialized and the test will fail.
+
+		DisposableServer disposableServer =  HttpServer.create()
+				.port(0)
+				.route(r -> r.get("/foo", (in, out) -> out.sendString(Flux.just("bar"))))
+				.bindNow();
+
+		assertThatCode(() -> HttpClient
+				.create(ConnectionProvider.builder("foo")
+						.metrics(true, NoOpMeterRegistrar::new)
+						.build())
+				.metrics(true, NoOpHttpClientMetricsRecorder::new)
+				.port(disposableServer.port())
+				.baseUrl("/foo")
+				.get()
+				.response()
+				.block()
+		).doesNotThrowAnyException();
+	}
+
+	private static class NoOpHttpClientMetricsRecorder implements ChannelMetricsRecorder {
+
+		@Override
+		public void recordDataReceived(SocketAddress remoteAddress, long bytes) {
+
+		}
+
+		@Override
+		public void recordDataSent(SocketAddress remoteAddress, long bytes) {
+
+		}
+
+		@Override
+		public void incrementErrorsCount(SocketAddress remoteAddress) {
+
+		}
+
+		@Override
+		public void recordTlsHandshakeTime(SocketAddress remoteAddress, Duration time, String status) {
+
+		}
+
+		@Override
+		public void recordConnectTime(SocketAddress remoteAddress, Duration time, String status) {
+
+		}
+
+		@Override
+		public void recordResolveAddressTime(SocketAddress remoteAddress, Duration time, String status) {
+
+		}
+	}
+
+	private static class NoOpMeterRegistrar implements ConnectionProvider.MeterRegistrar {
+
+		@Override
+		public void registerMetrics(String poolName, String id, SocketAddress remoteAddress,
+		                            ConnectionPoolMetrics metrics) {
+
+		}
+	}
+}
