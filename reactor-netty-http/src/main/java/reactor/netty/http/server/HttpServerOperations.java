61f12a5842e21174837d707ef3e7a33575c751d9
Violeta Georgieva <violetag@vmware.com>
2022-01-26 13:36:59 +0200

    HttpServerOperations#isFormUrlencoded handles header value with charset (#1998)
    Fixes #1988

--- reactor-netty-http/src/main/java/reactor/netty/http/server/HttpServerOperations.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/server/HttpServerOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2021 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2011-2022 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -294,8 +294,9 @@ class HttpServerOperations extends HttpOperations<HttpServerRequest, HttpServerR
 
 	@Override
 	public boolean isFormUrlencoded() {
-		String contentType = requestHeaders().get(HttpHeaderNames.CONTENT_TYPE);
-		return HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.contentEqualsIgnoreCase(contentType);
+		CharSequence mimeType = HttpUtil.getMimeType(nettyRequest);
+		return mimeType != null &&
+				HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.contentEqualsIgnoreCase(mimeType.toString().trim());
 	}
 
 	@Override
