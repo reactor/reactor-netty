ba35218f4c396cb2ad48e4dcbf3c49b0d9918938
Ryan Rupp <3022260+ryanrupp@users.noreply.github.com>
2022-01-28 06:02:41 -0600

    [doc] Extended TCP keep-alive settings (NIO transport) are only available on Mac/Linux not Windows (#1981)
    Co-authored-by: Violeta Georgieva <violetag@vmware.com>

--- reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/http/client/channeloptions/Application.java
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/http/client/channeloptions/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2020-2022 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,8 @@ public class Application {
 				          .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000) //<1>
 				          .option(ChannelOption.SO_KEEPALIVE, true)            //<2>
 				          // The options below are available only when NIO transport (Java 11) is used
+				          // on Mac or Linux (Java does not currently support these extended options on Windows)
+				          // https://bugs.openjdk.java.net/browse/JDK-8194298
 				          //.option(NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPIDLE), 300)
 				          //.option(NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPINTERVAL), 60)
 				          //.option(NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPCOUNT), 8);
