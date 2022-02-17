1dba14c93d9d257d0406df31bd22131dd7a9cdb4
Violeta Georgieva <violetag@vmware.com>
2022-02-17 08:26:55 +0200

    Update reactor-netty-examples module to use micrometer-api

--- reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/http/client/metrics/Application.java
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/documentation/http/client/metrics/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2020-2022 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,8 @@
  */
 package reactor.netty.examples.documentation.http.client.metrics;
 
-import io.micrometer.core.instrument.Metrics;
-import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.api.instrument.Metrics;
+import io.micrometer.api.instrument.config.MeterFilter;
 import reactor.netty.http.client.HttpClient;
 
 public class Application {
