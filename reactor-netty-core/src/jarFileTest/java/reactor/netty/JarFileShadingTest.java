b393dc98a4eb9f5e199b34f39bb6ee961d63aa1e
Violeta Georgieva <violetag@vmware.com>
2022-03-31 16:33:44 +0300

    [test] Handle use cases when Reactor Netty version is milestone or release candidate

--- reactor-netty-core/src/jarFileTest/java/reactor/netty/JarFileShadingTest.java
+++ reactor-netty-core/src/jarFileTest/java/reactor/netty/JarFileShadingTest.java
@@ -73,8 +73,8 @@ class JarFileShadingTest extends AbstractJarFileTest {
 				.replace("-original.jar", "")
 				.replace(".jar", "");
 
-		String osgiVersion = version.replace("-SNAPSHOT", ".BUILD-")
-				.replace("-M1", ".M1"); //FIXME accommodate all milestones and release candidates
+		String osgiVersion = version.replace('-', '.')
+				.replace(".SNAPSHOT", ".BUILD-");
 
 		try (InputStream inputStream = jar.getInputStream(manifest);
 		     BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, UTF_8))) {
