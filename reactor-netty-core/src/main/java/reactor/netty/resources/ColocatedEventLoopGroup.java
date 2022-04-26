ffd461c4bf605a280d4e350fbd670f8ea5c93c83
Violeta Georgieva <violetag@vmware.com>
2022-04-26 13:51:42 +0300

    Ensure PooledConnectionProvider check only for event loop in the thread local
    EventLoopGroup.next() should be called only in cases it will be used
    for example establishing a connection.
    Pool.acquire(...) in most cases will reuse the connections and in
    these cases EventLoopGroup.next() invocation is not needed.

--- reactor-netty-core/src/main/java/reactor/netty/resources/ColocatedEventLoopGroup.java
+++ reactor-netty-core/src/main/java/reactor/netty/resources/ColocatedEventLoopGroup.java
@@ -33,6 +33,7 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.util.concurrent.EventExecutor;
 import io.netty.util.concurrent.FastThreadLocal;
 import io.netty.util.concurrent.ScheduledFuture;
+import reactor.util.annotation.Nullable;
 
 /**
  * Reuse local event loop if already working inside one.
@@ -124,10 +125,8 @@ final class ColocatedEventLoopGroup implements EventLoopGroup, Supplier<EventLoo
 
 	@Override
 	public EventLoop next() {
-		if (localLoop.isSet()) {
-			return localLoop.get();
-		}
-		return eventLoopGroup.next();
+		EventLoop loop = nextInternal();
+		return loop != null ? loop : eventLoopGroup.next();
 	}
 
 	@Override
@@ -217,5 +216,10 @@ final class ColocatedEventLoopGroup implements EventLoopGroup, Supplier<EventLoo
 		for (EventExecutor ex : eventLoopGroup) {
 			ex.execute(() -> localLoop.set(null));
 		}
+	}
+
+	@Nullable
+	EventLoop nextInternal() {
+		return localLoop.isSet() ? localLoop.get() : null;
 	}
 }
