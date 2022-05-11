371e344e09c2daa52ca81ad4fbb82446e473d82c
Alexej Timonin <alexejtimonin@gmail.com>
2022-05-11 22:06:09 +0200

    Follow redirect on 303

--- reactor-netty-http/src/main/java/reactor/netty/http/client/HttpClient.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/client/HttpClient.java
@@ -905,7 +905,7 @@ public abstract class HttpClient extends ClientTransport<HttpClient, HttpClientC
 	}
 
 	/**
-	 * Specifies whether HTTP status 301|302|307|308 auto-redirect support is enabled.
+	 * Specifies whether HTTP status 301|302|303|307|308 auto-redirect support is enabled.
 	 *
 	 * <p><strong>Note:</strong> The sensitive headers {@link #followRedirect(boolean, Consumer) followRedirect}
 	 * are removed from the initialized request when redirecting to a different domain, they can be re-added
