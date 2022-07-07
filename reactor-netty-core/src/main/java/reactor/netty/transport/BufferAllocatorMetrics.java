3a10231c6d15db27519067ec85e1eec1af572d4c
Violeta Georgieva <violetag@vmware.com>
2022-07-07 19:34:22 +0300

    Adapt ByteBufAllocatorMetrics to Buffer API (#2363)
    Fixes #2341

---
+++ reactor-netty-core/src/main/java/reactor/netty/transport/BufferAllocatorMetrics.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2019-2022 VMware, Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.netty.transport;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.Tags;
+import io.netty5.buffer.api.BufferAllocator;
+import io.netty5.buffer.api.pool.BufferAllocatorMetric;
+import reactor.netty.internal.util.MapUtils;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import static reactor.netty.Metrics.REGISTRY;
+import static reactor.netty.transport.BufferAllocatorMeters.ACTIVE_MEMORY;
+import static reactor.netty.transport.BufferAllocatorMeters.BufferAllocatorMetersTags.ID;
+import static reactor.netty.transport.BufferAllocatorMeters.BufferAllocatorMetersTags.TYPE;
+import static reactor.netty.transport.BufferAllocatorMeters.CHUNK_SIZE;
+import static reactor.netty.transport.BufferAllocatorMeters.ARENAS;
+import static reactor.netty.transport.BufferAllocatorMeters.NORMAL_CACHE_SIZE;
+import static reactor.netty.transport.BufferAllocatorMeters.SMALL_CACHE_SIZE;
+import static reactor.netty.transport.BufferAllocatorMeters.THREAD_LOCAL_CACHES;
+import static reactor.netty.transport.BufferAllocatorMeters.USED_MEMORY;
+
+/**
+ * @author Violeta Georgieva
+ * @since 0.9
+ */
+final class BufferAllocatorMetrics {
+
+	static final BufferAllocatorMetrics INSTANCE = new BufferAllocatorMetrics();
+
+	final ConcurrentMap<String, BufferAllocatorMetric> cache = new ConcurrentHashMap<>();
+
+	private BufferAllocatorMetrics() {
+	}
+
+	void registerMetrics(String allocType, BufferAllocatorMetric metrics, BufferAllocator alloc) {
+		MapUtils.computeIfAbsent(cache, metrics.hashCode() + "", key -> {
+			Tags tags = Tags.of(ID.getKeyName(), key, TYPE.getKeyName(), allocType);
+
+			Gauge.builder(USED_MEMORY.getName(), metrics, BufferAllocatorMetric::usedMemory)
+			     .tags(tags)
+			     .register(REGISTRY);
+
+			Gauge.builder(ARENAS.getName(), metrics, BufferAllocatorMetric::numArenas)
+			     .tags(tags)
+			     .register(REGISTRY);
+
+			Gauge.builder(THREAD_LOCAL_CACHES.getName(), metrics, BufferAllocatorMetric::numThreadLocalCaches)
+			     .tags(tags)
+			     .register(REGISTRY);
+
+			Gauge.builder(SMALL_CACHE_SIZE.getName(), metrics, BufferAllocatorMetric::smallCacheSize)
+			     .tags(tags)
+			     .register(REGISTRY);
+
+			Gauge.builder(NORMAL_CACHE_SIZE.getName(), metrics, BufferAllocatorMetric::normalCacheSize)
+			     .tags(tags)
+			     .register(REGISTRY);
+
+			Gauge.builder(CHUNK_SIZE.getName(), metrics, BufferAllocatorMetric::chunkSize)
+			     .tags(tags)
+			     .register(REGISTRY);
+
+			Gauge.builder(ACTIVE_MEMORY.getName(), metrics, BufferAllocatorMetric::pinnedMemory)
+			     .tags(tags)
+			     .register(REGISTRY);
+
+			return metrics;
+		});
+	}
+}
