4938a5aa4e76a605417cf3a60a3026f5e650cc0b
Violeta Georgieva <violetag@vmware.com>
2022-10-20 15:22:14 +0300

    Polish contribution

--- reactor-netty-core/src/test/java/reactor/netty/tcp/TcpClientTests.java
+++ reactor-netty-core/src/test/java/reactor/netty/tcp/TcpClientTests.java
@@ -258,11 +258,9 @@ public class TcpClientTests {
 	}
 
 	@Test
-	void tcpConnectionInboundAndOutboundCompletesOnDispose() throws InterruptedException {
-		final CountDownLatch latch = new CountDownLatch(100);
-		final TestSubscriber<Object> outboundSubscriber = TestSubscriber.create();
-		final TestSubscriber<Object> inboundSubscriber = TestSubscriber.create();
-		final Sinks.Empty<Void> awaiter = Sinks.empty();
+	void tcpConnectionInboundAndOutboundCompletesOnDispose() throws Exception {
+		final CountDownLatch latch1 = new CountDownLatch(100);
+		final CountDownLatch latch2 = new CountDownLatch(2);
 
 		Connection client =
 				TcpClient.create()
@@ -270,30 +268,31 @@ public class TcpClientTests {
 				         .port(echoServerPort)
 				         .doOnConnected(c -> c.addHandlerLast("codec", new LineBasedFrameDecoder(8 * 1024)))
 				         .handle((in, out) -> {
-					         out.sendString(Flux.interval(Duration.ofMillis(10))
-					                            .map(i -> "Hello World!" + i + "\n")
-					                            .subscribeOn(Schedulers.parallel()))
-					            .then()
-					            .subscribe(outboundSubscriber);
+				             out.sendString(Flux.interval(Duration.ofMillis(10))
+				                                .map(i -> "Hello World!" + i + "\n")
+				                                .subscribeOn(Schedulers.parallel()))
+				                .then()
+				                .doOnTerminate(latch2::countDown)
+				                .subscribe();
 
-					         in.receive()
-					           .asString()
-					           .flatMapIterable(s -> Arrays.asList(s.split("\\n")))
-					           .doOnNext(s -> latch.countDown())
-					           .then()
-					           .subscribe(inboundSubscriber);
+				             in.receive()
+				               .asString()
+				               .flatMapIterable(s -> Arrays.asList(s.split("\\n")))
+				               .doOnNext(s -> latch1.countDown())
+				               .then()
+				               .doOnTerminate(latch2::countDown)
+				               .subscribe();
 
-							 return awaiter.asMono();
+				             return out.neverComplete();
 				         })
 				         .wiretap(true)
 				         .connectNow(Duration.ofSeconds(15));
 
-		assertThat(latch.await(15, TimeUnit.SECONDS)).isTrue();
+		assertThat(latch1.await(15, TimeUnit.SECONDS)).isTrue();
 
 		client.disposeNow();
 
-		assertThat(inboundSubscriber.isTerminatedComplete()).isTrue();
-		assertThat(outboundSubscriber.isTerminatedComplete()).isTrue();
+		assertThat(latch2.await(15, TimeUnit.SECONDS)).isTrue();
 	}
 
 	@Test
