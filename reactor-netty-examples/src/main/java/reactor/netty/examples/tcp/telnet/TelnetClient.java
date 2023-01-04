00e3fdb59b41c5fa0d9569a6d3813ed698b0e47e
Violeta Georgieva <violetag@vmware.com>
2023-01-04 14:48:05 +0200

    Polish formatting

--- reactor-netty-examples/src/main/java/reactor/netty/examples/tcp/telnet/TelnetClient.java
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/tcp/telnet/TelnetClient.java
@@ -27,43 +27,49 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class TelnetClient {
+
 	static final boolean SECURE = System.getProperty("secure") != null;
 	static final String HOST = System.getProperty("host", "127.0.0.1");
 	static final int PORT = Integer.parseInt(System.getProperty("port", SECURE ? "8992" : "8993"));
 	static final boolean WIRETAP = System.getProperty("wiretap") != null;
 
 	public static void main(String... args) {
-
-
-		TcpClient client = TcpClient.create().host(HOST).port(PORT)
-				.doOnConnected(connection -> {
-					connection.addHandlerLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
-				})
-				.wiretap(WIRETAP);
+		TcpClient client =
+				TcpClient.create()
+				         .host(HOST)
+				         .port(PORT)
+				         .doOnConnected(connection ->
+				             connection.addHandlerLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter())))
+				         .wiretap(WIRETAP);
 
 		if (SECURE) {
-			TcpSslContextSpec tcpSslContextSpec = TcpSslContextSpec.forClient()
-					.configure(builder -> builder.trustManager(InsecureTrustManagerFactory.INSTANCE));
+			TcpSslContextSpec tcpSslContextSpec =
+					TcpSslContextSpec.forClient()
+					                 .configure(builder -> builder.trustManager(InsecureTrustManagerFactory.INSTANCE));
 			client = client.secure(spec -> spec.sslContext(tcpSslContextSpec));
 		}
 
 		Connection conn = client.connectNow();
-		conn.inbound().receive()
-				.asString(StandardCharsets.UTF_8)
-				.doOnNext(System.out::println)
-				.subscribe();
+
+		conn.inbound()
+		    .receive()
+		    .asString(StandardCharsets.UTF_8)
+		    .doOnNext(System.out::println)
+		    .subscribe();
 
 		Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8.name());
 		while (scanner.hasNext()) {
 			String text = scanner.nextLine();
-			conn.outbound().sendString(Mono.just(text + "\r\n"))
-					.then()
-					.subscribe();
+			conn.outbound()
+			    .sendString(Mono.just(text + "\r\n"))
+			    .then()
+			    .subscribe();
 			if ("bye".equalsIgnoreCase(text)) {
 				break;
 			}
 		}
 
-		conn.onDispose().block();
+		conn.onDispose()
+		    .block();
 	}
 }
--- reactor-netty-examples/src/main/java/reactor/netty/examples/tcp/telnet/TelnetServer.java
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/tcp/telnet/TelnetServer.java
@@ -31,51 +31,51 @@ public class TelnetServer {
 
 	static final boolean SECURE = System.getProperty("secure") != null;
 	static final int PORT = Integer.parseInt(System.getProperty("port", SECURE ? "8992" : "8993"));
-
 	static final boolean WIRETAP = System.getProperty("wiretap") != null;
 
 	public static void main(String[] args) throws CertificateException, UnknownHostException {
 		String hostName = InetAddress.getLocalHost().getHostName();
 
-		TcpServer server = TcpServer.create()
-				.port(PORT)
-				.wiretap(WIRETAP)
-				.doOnConnection(connection -> {
-					connection.addHandlerLast(new DelimiterBasedFrameDecoder(8092, Delimiters.lineDelimiter()));
-				})
-				.handle((in, out) -> {
-							Flux<String> welcomeFlux = Flux.just("Welcome to " + hostName + "!\r\n", "It is " + new Date() + " now.\r\n");
-							Flux<String> responses =
-									in.receive()
-											.asString()
-											// Signals completion when 'bye' is encountered.
-											// Reactor Netty will perform the necessary clean up, including
-											// disposing the channel.
-											.takeUntil("bye"::equalsIgnoreCase)
-											.map(text -> {
-												String response = "Did you say '" + text + "'?\r\n";
-												if (text.isEmpty()) {
-													response = "Please type something.\r\n";
-												}
-												else if ("bye".equalsIgnoreCase(text)) {
-													response = "Have a good day!\r\n";
-												}
+		TcpServer server =
+				TcpServer.create()
+				         .port(PORT)
+				         .wiretap(WIRETAP)
+				         .doOnConnection(connection ->
+				             connection.addHandlerLast(new DelimiterBasedFrameDecoder(8092, Delimiters.lineDelimiter())))
+				         .handle((in, out) -> {
+				             Flux<String> welcomeFlux =
+				                     Flux.just("Welcome to " + hostName + "!\r\n", "It is " + new Date() + " now.\r\n");
 
-												return response;
-											});
+				             Flux<String> responses =
+				                     in.receive()
+				                       .asString()
+				                       // Signals completion when 'bye' is encountered.
+				                       // Reactor Netty will perform the necessary clean up, including
+				                       // disposing the channel.
+				                       .takeUntil("bye"::equalsIgnoreCase)
+				                       .map(text -> {
+				                           String response = "Did you say '" + text + "'?\r\n";
+				                           if (text.isEmpty()) {
+				                               response = "Please type something.\r\n";
+				                           }
+				                           else if ("bye".equalsIgnoreCase(text)) {
+				                               response = "Have a good day!\r\n";
+				                           }
 
-							return out.sendString(Flux.concat(welcomeFlux, responses));
-						}
-				);
+				                           return response;
+				                       });
+
+				             return out.sendString(Flux.concat(welcomeFlux, responses));
+				         });
 
 		if (SECURE) {
 			SelfSignedCertificate ssc = new SelfSignedCertificate();
-			server = server.secure(spec -> spec.sslContext(TcpSslContextSpec.forServer(ssc.certificate(), ssc.privateKey())));
+			server = server.secure(
+					spec -> spec.sslContext(TcpSslContextSpec.forServer(ssc.certificate(), ssc.privateKey())));
 		}
 
 		server.bindNow()
-				.onDispose()
-				.block();
-
+		      .onDispose()
+		      .block();
 	}
 }
