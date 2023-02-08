6e6a15ef085aa6e1da7a78ef646c02d3371d8512
Violeta Georgieva <violetag@vmware.com>
2023-02-08 08:18:01 +0200

    Polish contribution

--- reactor-netty-examples/src/main/java/reactor/netty/examples/tcp/securechat/SecureChatClient.java
+++ reactor-netty-examples/src/main/java/reactor/netty/examples/tcp/securechat/SecureChatClient.java
@@ -29,42 +29,43 @@ import reactor.netty.tcp.TcpSslContextSpec;
 public class SecureChatClient {
 
 	private static final String HOST = System.getProperty("host", "127.0.0.1");
-
 	private static final int PORT = Integer.parseInt(System.getProperty("port", "8992"));
-
 	private static final boolean WIRETAP = System.getProperty("wiretap") != null;
 
 	public static void main(String[] args) {
-		TcpSslContextSpec tcpSslContextSpec = TcpSslContextSpec.forClient()
-				.configure(builder -> builder.trustManager(InsecureTrustManagerFactory.INSTANCE));
+		TcpSslContextSpec tcpSslContextSpec =
+				TcpSslContextSpec.forClient()
+				                 .configure(builder -> builder.trustManager(InsecureTrustManagerFactory.INSTANCE));
 
-		TcpClient client = TcpClient.create()
-				.host(HOST)
-				.port(PORT)
-				.wiretap(WIRETAP)
-				.doOnConnected(connection -> {
-					connection.addHandlerLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
-				}).secure(spec -> spec.sslContext(tcpSslContextSpec));
+		TcpClient client =
+				TcpClient.create()
+				         .host(HOST)
+				         .port(PORT)
+				         .wiretap(WIRETAP)
+				         .doOnConnected(connection ->
+				             connection.addHandlerLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter())))
+				         .secure(spec -> spec.sslContext(tcpSslContextSpec));
 
 		Connection conn = client.connectNow();
+
 		conn.inbound()
-				.receive()
-				.asString()
-				.subscribe(System.out::println);
+		    .receive()
+		    .asString()
+		    .subscribe(System.out::println);
 
 		Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8.name());
 		while (scanner.hasNext()) {
 			String text = scanner.nextLine();
-			conn.outbound().sendString(Mono.just(text + "\r\n"))
-					.then()
-					.subscribe();
-
+			conn.outbound()
+			    .sendString(Mono.just(text + "\r\n"))
+			    .then()
+			    .subscribe();
 			if ("bye".equalsIgnoreCase(text)) {
 				break;
 			}
 		}
 
-		conn.onDispose().block();
-
+		conn.onDispose()
+		    .block();
 	}
 }
