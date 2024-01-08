e0e4c5dea1a617950fe207642ad8378654fcc75f
Violeta Georgieva <violeta.georgieva@broadcom.com>
2024-01-08 19:54:50 +0200

    Polish

--- reactor-netty-core/src/main/java/reactor/netty/channel/ContextAwareChannelMetricsHandler.java
+++ reactor-netty-core/src/main/java/reactor/netty/channel/ContextAwareChannelMetricsHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2021-2024 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -133,6 +133,7 @@ final class ContextAwareChannelMetricsHandler extends AbstractChannelMetricsHand
 	}
 
 	static final class TlsMetricsHandler extends ChannelMetricsHandler.TlsMetricsHandler {
+
 		TlsMetricsHandler(ContextAwareChannelMetricsRecorder recorder) {
 			super(recorder);
 		}
