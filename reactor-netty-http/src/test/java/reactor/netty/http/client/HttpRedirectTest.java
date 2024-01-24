reactor/reactor-netty

commit 44a7e89c794380671a79445547e34007ca115941

Ensure RFC 6265 compliance when sending multiple cookies (#2994)
According to RFC 6265

"When the user agent generates an HTTP request, the user agent MUST
NOT attach more than one Cookie header field."

https://datatracker.ietf.org/doc/html/rfc6265#section-5.4

Fixes #2983

reactor-netty-http/src/main/java/reactor/netty/http/client/HttpClientOperations.java
@@ -20,7 +20,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.channels.ClosedChannelException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -106,6 +108,7 @@ class HttpClientOperations extends HttpOperations<NettyInbound, NettyOutbound>
 	final HttpHeaders            requestHeaders;
 	final ClientCookieEncoder    cookieEncoder;
 	final ClientCookieDecoder    cookieDecoder;
+	final List<Cookie>           cookieList;
 	final Sinks.One<HttpHeaders> trailerHeaders;
 
 	Supplier<String>[]          redirectedFrom = EMPTY_REDIRECTIONS;
@@ -144,6 +147,7 @@ class HttpClientOperations extends HttpOperations<NettyInbound, NettyOutbound>
 		this.requestHeaders = replaced.requestHeaders;
 		this.cookieEncoder = replaced.cookieEncoder;
 		this.cookieDecoder = replaced.cookieDecoder;
+		this.cookieList = replaced.cookieList;
 		this.resourceUrl = replaced.resourceUrl;
 		this.path = replaced.path;
 		this.responseTimeout = replaced.responseTimeout;
@@ -165,14 +169,14 @@ class HttpClientOperations extends HttpOperations<NettyInbound, NettyOutbound>
 		this.requestHeaders = nettyRequest.headers();
 		this.cookieDecoder = decoder;
 		this.cookieEncoder = encoder;
+		this.cookieList = new ArrayList<>();
 		this.trailerHeaders = Sinks.unsafe().one();
 	}
 
 	@Override
 	public HttpClientRequest addCookie(Cookie cookie) {
 		if (!hasSentHeaders()) {
-			this.requestHeaders.add(HttpHeaderNames.COOKIE,
-					cookieEncoder.encode(cookie));
+			this.cookieList.add(cookie);
 		}
 		else {
 			throw new IllegalStateException("Status and headers already sent");
@@ -587,6 +591,10 @@ class HttpClientOperations extends HttpOperations<NettyInbound, NettyOutbound>
 
 	@Override
 	protected void beforeMarkSentHeaders() {
+		if (!cookieList.isEmpty()) {
+			requestHeaders.add(HttpHeaderNames.COOKIE, cookieEncoder.encode(cookieList));
+		}
+
 		if (redirectedFrom.length > 0) {
 			if (redirectRequestConsumer != null) {
 				redirectRequestConsumer.accept(this);

reactor-netty-http/src/test/java/reactor/netty/http/HttpCookieHandlingTests.java
@@ -17,11 +17,13 @@ package reactor.netty.http;
 
 import java.time.Duration;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 
 import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.cookie.ClientCookieDecoder;
 import io.netty.handler.codec.http.cookie.ClientCookieEncoder;
 import io.netty.handler.codec.http.cookie.Cookie;
@@ -161,6 +163,40 @@ class HttpCookieHandlingTests extends BaseHttpTest {
 		        .asString()
 		        .as(StepVerifier::create)
 		        .expectNext(expectedResponse)
+		        .expectComplete()
+		        .verify(Duration.ofSeconds(5));
+	}
+
+	@Test
+	void testIssue2983() {
+		disposableServer =
+				createServer()
+				        .handle((req, res) -> {
+				            List<String> cookies = req.requestHeaders().getAll(HttpHeaderNames.COOKIE);
+				            return cookies.size() == 1 ? res.sendString(Mono.just(cookies.get(0))) :
+				                    res.sendString(Mono.just("ERROR"));
+				        })
+				        .bindNow();
+
+		createClient(disposableServer.port())
+		        .request(HttpMethod.GET)
+		        .uri("/")
+		        .send((req, out) -> {
+		            Cookie cookie1 = new DefaultCookie("testIssue2983_1", "1");
+		            cookie1.setPath("/");
+		            Cookie cookie2 = new DefaultCookie("testIssue2983_2", "2");
+		            cookie2.setPath("/2");
+		            Cookie cookie3 = new DefaultCookie("testIssue2983_3", "3");
+		            req.addCookie(cookie1)
+		               .addCookie(cookie2)
+		               .addCookie(cookie3);
+		            return out;
+		        })
+		        .responseContent()
+		        .aggregate()
+		        .asString()
+		        .as(StepVerifier::create)
+		        .expectNext("testIssue2983_3=3; testIssue2983_2=2; testIssue2983_1=1")
 		        .expectComplete()
 		        .verify(Duration.ofSeconds(5));
 	}

commit 2852baed0876481bfd74609eda4da6052e457579

Ensure cookies can be added by BiConsumer provided with HttpClient#followRedirect
This is a regression caused by PR #2994

Fixes #3035

reactor-netty-http/src/main/java/reactor/netty/http/client/HttpClientOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2023 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2011-2024 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -591,10 +591,6 @@ class HttpClientOperations extends HttpOperations<NettyInbound, NettyOutbound>
 
 	@Override
 	protected void beforeMarkSentHeaders() {
-		if (!cookieList.isEmpty()) {
-			requestHeaders.add(HttpHeaderNames.COOKIE, cookieEncoder.encode(cookieList));
-		}
-
 		if (redirectedFrom.length > 0) {
 			if (redirectRequestConsumer != null) {
 				redirectRequestConsumer.accept(this);
@@ -603,6 +599,10 @@ class HttpClientOperations extends HttpOperations<NettyInbound, NettyOutbound>
 				redirectRequestBiConsumer.accept(previousRequestHeaders, this);
 				previousRequestHeaders = null;
 			}
+		}
+
+		if (!cookieList.isEmpty()) {
+			requestHeaders.add(HttpHeaderNames.COOKIE, cookieEncoder.encode(cookieList));
 		}
 	}
 

reactor-netty-http/src/test/java/reactor/netty/http/client/HttpRedirectTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2023 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2017-2024 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,6 +34,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.cookie.DefaultCookie;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
@@ -756,6 +757,31 @@ class HttpRedirectTest extends BaseHttpTest {
 		        .asString()
 		        .as(StepVerifier::create)
 		        .expectNext("testIssue2670")
+		        .expectComplete()
+		        .verify(Duration.ofSeconds(5));
+	}
+
+	@Test
+	@SuppressWarnings("CollectionUndefinedEquality")
+	void testIssue3035() {
+		disposableServer =
+				createServer()
+				        .route(r -> r.get("/1", (req, res) -> res.sendRedirect("/2"))
+				                     .get("/2", (req, res) ->
+				                             req.cookies().containsKey("testCookie") ?
+				                                     res.status(200).sendString(Mono.just("OK")) :
+				                                     res.status(400).sendString(Mono.just("KO"))))
+				        .bindNow();
+
+		createClient(disposableServer::address)
+		        .followRedirect(
+		                (req, res) -> res.status().code() == 302,
+		                (headers, redirect) -> redirect.addCookie(new DefaultCookie("testCookie", "testCookie")))
+		        .get()
+		        .uri("/1")
+		        .responseSingle((res, bytes) -> bytes.asString().zipWith(Mono.just(res.status().code())))
+		        .as(StepVerifier::create)
+		        .expectNextMatches(tuple -> "OK".equals(tuple.getT1()) && tuple.getT2() == 200)
 		        .expectComplete()
 		        .verify(Duration.ofSeconds(5));
 	}