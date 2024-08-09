fbfcf31dcf0dd5a53924a626fa9945229ed57a90
Violeta Georgieva <violeta.georgieva@broadcom.com>
2024-08-09 16:06:53 +0300

    Simplify response status code check

--- reactor-netty-http/src/main/java/reactor/netty/http/server/AbstractHttpServerMetricsHandler.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/server/AbstractHttpServerMetricsHandler.java
@@ -157,7 +157,7 @@ abstract class AbstractHttpServerMetricsHandler extends ChannelDuplexHandler {
 	public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
 		try {
 			if (msg instanceof HttpResponse) {
-				if (((HttpResponse) msg).status().equals(HttpResponseStatus.CONTINUE)) {
+				if (((HttpResponse) msg).status().code() == HttpResponseStatus.CONTINUE.code()) {
 					//"FutureReturnValueIgnored" this is deliberate
 					ctx.write(msg, promise);
 					return;
