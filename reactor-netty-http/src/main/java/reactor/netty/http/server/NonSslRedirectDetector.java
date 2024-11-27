ecea19cb8c2189ea727ad58b692c11b9075502da
Violeta Georgieva <violeta.georgieva@broadcom.com>
2024-11-27 19:40:01 +0200

    Use SslHandler#isEncrypted(ByteBuf, boolean) instead of the deprecated SslHandler#isEncrypted(ByteBuf)

--- reactor-netty-http/src/main/java/reactor/netty/http/server/NonSslRedirectDetector.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/server/NonSslRedirectDetector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2021-2024 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ final class NonSslRedirectDetector extends ByteToMessageDecoder {
 			return;
 		}
 		ChannelPipeline pipeline = ctx.pipeline();
-		if (SslHandler.isEncrypted(in)) {
+		if (SslHandler.isEncrypted(in, false)) {
 			sslProvider.addSslHandler(ctx.channel(), remoteAddress, sslDebug);
 		}
 		else {
