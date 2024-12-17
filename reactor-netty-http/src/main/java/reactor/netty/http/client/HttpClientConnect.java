reactor/reactor-netty

commit 4c835a3700e71aaff3894e75e1257e151a234ff4

Ensure body is sent when DELETE and Transfer-Encoding (#3481)
According to the spec https://www.rfc-editor.org/rfc/rfc9110.html#name-delete

```
A client SHOULD NOT generate content in a DELETE request unless it is made
directly to an origin server that has previously indicated, in or out of band, that
such a request has a purpose and will be adequately supported.
```

When a user sends DELETE with content this means there is an indication that
the server supports this type of requests.

reactor-netty-http/src/main/java/reactor/netty/http/client/HttpClientConnect.java
@@ -559,7 +559,6 @@ class HttpClientConnect extends HttpClient {
 
 				if (!Objects.equals(method, HttpMethod.GET) &&
 							!Objects.equals(method, HttpMethod.HEAD) &&
-							!Objects.equals(method, HttpMethod.DELETE) &&
 							!headers.contains(HttpHeaderNames.CONTENT_LENGTH)) {
 					ch.chunkedTransfer(true);
 				}

reactor-netty-http/src/test/java/reactor/netty/http/client/HttpClientTest.java
@@ -98,6 +98,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -3444,5 +3445,28 @@ class HttpClientTest extends BaseHttpTest {
 		      .as(StepVerifier::create)
 		      .expectComplete()
 		      .verify(Duration.ofSeconds(5));
+	}
+
+	@ParameterizedTest
+	@ValueSource(booleans = {true, false})
+	void testDeleteMethod(boolean chunked) {
+		disposableServer =
+				createServer()
+				        .handle((req, res) -> res.send(req.receive().retain()))
+				        .bindNow();
+
+		Publisher<ByteBuf> body = chunked ?
+				ByteBufFlux.fromString(Flux.just("d", "e", "l", "e", "t", "e")) :
+				ByteBufMono.fromString(Mono.just("delete"));
+
+		createClient(disposableServer.port())
+		        .delete()
+		        .uri("/")
+		        .send(body)
+		        .responseSingle((res, bytes) -> bytes.asString())
+		        .as(StepVerifier::create)
+		        .expectNext("delete")
+		        .expectComplete()
+		        .verify(Duration.ofSeconds(5));
 	}
 }

commit 44d950420380842b151098e3436e9051a7324eaf

Do not add Transfer-Encoding for DELETE with no body
This is a regression caused by #3481

Fixes #3548

reactor-netty-http/src/main/java/reactor/netty/http/client/HttpClientConnect.java
@@ -576,8 +576,9 @@ class HttpClientConnect extends HttpClient {
 				ch.followRedirectPredicate(followRedirectPredicate);
 
 				if (!Objects.equals(method, HttpMethod.GET) &&
-							!Objects.equals(method, HttpMethod.HEAD) &&
-							!headers.contains(HttpHeaderNames.CONTENT_LENGTH)) {
+						!Objects.equals(method, HttpMethod.HEAD) &&
+						!Objects.equals(method, HttpMethod.DELETE) &&
+						!headers.contains(HttpHeaderNames.CONTENT_LENGTH)) {
 					ch.chunkedTransfer(true);
 				}
 

reactor-netty-http/src/main/java/reactor/netty/http/client/HttpClientOperations.java
@@ -445,7 +445,9 @@ class HttpClientOperations extends HttpOperations<NettyInbound, NettyOutbound>
 		if (source instanceof Mono) {
 			return super.send(source);
 		}
-		if (Objects.equals(method(), HttpMethod.GET) || Objects.equals(method(), HttpMethod.HEAD)) {
+		if (Objects.equals(method(), HttpMethod.GET) ||
+				Objects.equals(method(), HttpMethod.HEAD) ||
+				Objects.equals(method(), HttpMethod.DELETE)) {
 
 			ByteBufAllocator alloc = channel().alloc();
 			return new PostHeadersNettyOutbound(Flux.from(source)
@@ -480,7 +482,7 @@ class HttpClientOperations extends HttpOperations<NettyInbound, NettyOutbound>
 				                }
 				                for (ByteBuf bb : list) {
 				                	if (log.isDebugEnabled()) {
-						                log.debug(format(channel(), "Ignoring accumulated bytebuf on http GET {}"), bb);
+						                log.debug(format(channel(), "Ignoring accumulated bytebuf on http {} {}"), method(), bb);
 					                }
 				                	bb.release();
 				                }

reactor-netty-http/src/test/java/reactor/netty/http/client/HttpClientTest.java
@@ -3550,16 +3550,18 @@ class HttpClientTest extends BaseHttpTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(booleans = {true, false})
-	void testDeleteMethod(boolean chunked) {
+	@ValueSource(strings = {"mono", "flux", "empty"})
+	void testDeleteMethod(String requestBodyType) {
 		disposableServer =
 				createServer()
-				        .handle((req, res) -> res.send(req.receive().retain()))
+				        .handle((req, res) -> res.sendString(
+				            Flux.concat(req.receive().retain().aggregate().asString().defaultIfEmpty("empty"),
+				                        Mono.just(" " + req.requestHeaders().get(HttpHeaderNames.CONTENT_LENGTH)))))
 				        .bindNow();
 
-		Publisher<ByteBuf> body = chunked ?
+		Publisher<ByteBuf> body = "flux".equals(requestBodyType) ?
 				ByteBufFlux.fromString(Flux.just("d", "e", "l", "e", "t", "e")) :
-				ByteBufMono.fromString(Mono.just("delete"));
+				"mono".equals(requestBodyType) ? ByteBufMono.fromString(Mono.just("delete")) : Mono.empty();
 
 		createClient(disposableServer.port())
 		        .delete()
@@ -3567,7 +3569,7 @@ class HttpClientTest extends BaseHttpTest {
 		        .send(body)
 		        .responseSingle((res, bytes) -> bytes.asString())
 		        .as(StepVerifier::create)
-		        .expectNext("delete")
+		        .expectNext("empty".equals(requestBodyType) ? "empty 0" : "delete 6")
 		        .expectComplete()
 		        .verify(Duration.ofSeconds(5));
 	}