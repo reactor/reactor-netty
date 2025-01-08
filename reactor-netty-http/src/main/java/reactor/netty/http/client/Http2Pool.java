af2a62ea5ae2681cc14d1cb04e70e8463f5dc409
Violeta Georgieva <violeta.georgieva@broadcom.com>
2025-01-08 09:28:14 +0200

    Http2Pool: Do not proceed with the resource delivery if Borrower is cancelled
    Signed-off-by: Violeta Georgieva <violeta.georgieva@broadcom.com>

--- reactor-netty-http/src/main/java/reactor/netty/http/client/Http2Pool.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/client/Http2Pool.java
@@ -375,7 +375,7 @@ final class Http2Pool implements InstrumentedPool<Connection>, InstrumentedPool.
 				Slot slot = belowMinConnections ? null : findConnection(resources);
 				if (slot != null) {
 					Borrower borrower = pollPending(borrowers, true);
-					if (borrower == null) {
+					if (borrower == null || borrower.get()) {
 						offerSlot(resources, slot);
 						continue;
 					}
@@ -420,7 +420,7 @@ final class Http2Pool implements InstrumentedPool<Connection>, InstrumentedPool.
 								poolConfig.allocationStrategy().returnPermits(permits - 1);
 							}
 							Borrower borrower = pollPending(borrowers, true);
-							if (borrower == null) {
+							if (borrower == null || borrower.get()) {
 								continue;
 							}
 							if (isDisposed()) {
