94089027be346a13df7eb1bbafb1294c40a66490
Violeta Georgieva <violeta.georgieva@broadcom.com>
2025-01-21 15:58:50 +0200

    Use DnsNameResolverBuilder#datagramChannelFactory instead of the deprecated DnsNameResolverBuilder#channelFactory (#3598)
    Signed-off-by: Violeta Georgieva <violeta.georgieva@broadcom.com>

--- reactor-netty-core/src/main/java/reactor/netty/transport/NameResolverProvider.java
+++ reactor-netty-core/src/main/java/reactor/netty/transport/NameResolverProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2024 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2020-2025 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -573,7 +573,7 @@ public final class NameResolverProvider {
 				.ndots(ndots)
 				.queryTimeoutMillis(queryTimeout.toMillis())
 				.eventLoop(group.next())
-				.channelFactory(() -> loop.onChannel(DatagramChannel.class, group))
+				.datagramChannelFactory(() -> loop.onChannel(DatagramChannel.class, group))
 				.socketChannelFactory(() -> loop.onChannel(SocketChannel.class, group), retryTcpOnTimeout);
 		if (bindAddressSupplier != null) {
 			// There is no check for bindAddressSupplier.get() == null
