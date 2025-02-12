2b8b4739f46776de31b5f3d72e38e67f71564c15
Violeta Georgieva <violeta.georgieva@broadcom.com>
2025-02-12 19:07:22 +0200

    Add Nullable annotation to overridden method (#3626)
    Signed-off-by: Violeta Georgieva <violeta.georgieva@broadcom.com>

--- reactor-netty-http/src/main/java/reactor/netty/http/client/HttpConnectionProvider.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/client/HttpConnectionProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2022 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2020-2025 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,6 +67,7 @@ final class HttpConnectionProvider implements ConnectionProvider {
 	}
 
 	@Override
+	@Nullable
 	public Map<SocketAddress, Integer> maxConnectionsPerHost() {
 		return http1ConnectionProvider().maxConnectionsPerHost();
 	}
--- reactor-netty-http/src/main/java/reactor/netty/http/client/MicrometerHttpClientMetricsHandler.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/client/MicrometerHttpClientMetricsHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2024 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2022-2025 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -188,6 +188,7 @@ final class MicrometerHttpClientMetricsHandler extends AbstractHttpClientMetrics
 		}
 
 		@Override
+		@Nullable
 		public Timer getTimer() {
 			return recorder.getResponseTimeTimer(getName(), netPeerName + ":" + netPeerPort, proxyAddress == null ? NA : proxyAddress, path, method, status);
 		}
