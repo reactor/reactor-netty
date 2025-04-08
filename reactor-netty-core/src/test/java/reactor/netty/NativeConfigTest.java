fa57e5180b4a4918f034299aeb9b5a44be22986c
Violeta Georgieva <696661+violetagg@users.noreply.github.com>
2025-04-08 19:33:00 +0300

    Temporary fix for missing graalvm metadata
    Signed-off-by: Violeta Georgieva <696661+violetagg@users.noreply.github.com>

--- reactor-netty-core/src/test/java/reactor/netty/NativeConfigTest.java
+++ reactor-netty-core/src/test/java/reactor/netty/NativeConfigTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2024 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2022-2025 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package reactor.netty;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.channel.ChannelHandler;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.reflections.util.ConfigurationBuilder;
@@ -32,6 +33,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assumptions.assumeThat;
 import static org.reflections.scanners.Scanners.SubTypes;
 
+@Disabled
 class NativeConfigTest {
 
 	@Test
