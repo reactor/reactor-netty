b22544669b7e2322e9887354deaf6071c6d6781d
raccoonback <kosb15@naver.com>
2025-04-14 22:15:53 +0900

    Extend tests to cover HTTP/1.1, HTTP/2, and HTTP/3 protocols
    Signed-off-by: raccoonback <kosb15@naver.com>

--- reactor-netty-http/src/test/java/reactor/netty/http/server/logging/error/ErrorLogTest.java
+++ reactor-netty-http/src/test/java/reactor/netty/http/server/logging/error/ErrorLogTest.java
@@ -19,15 +19,29 @@ import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.core.Appender;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 import reactor.netty.BaseHttpTest;
+import reactor.netty.http.Http11SslContextSpec;
+import reactor.netty.http.Http2SslContextSpec;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.client.HttpClientConfig;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.http.server.HttpServerConfig;
+import reactor.util.function.Tuple2;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static reactor.netty.http.server.logging.error.ErrorLog.LOGGER;
@@ -59,9 +73,10 @@ class ErrorLogTest extends BaseHttpTest {
 		ROOT.detachAppender(mockedAppender);
 	}
 
-	@Test
-	void errorLogDefaultFormat() {
-		disposableServer = createServer()
+	@ParameterizedTest
+	@MethodSource("dataCompatibleCombinations")
+	void errorLogDefaultFormat(HttpServer httpServer, HttpClient httpClient) {
+		disposableServer = httpServer
 				.handle((req, resp) -> {
 					resp.withConnection(
 							conn -> conn.channel()
@@ -73,7 +88,7 @@ class ErrorLogTest extends BaseHttpTest {
 				.errorLog(true)
 				.bindNow();
 
-		getHttpClientResponse("/example/test");
+		getHttpClientResponse(httpClient, "/example/test");
 
 		Mockito.verify(mockedAppender, Mockito.times(1))
 				.doAppend(loggingEventArgumentCaptor.capture());
@@ -86,9 +101,10 @@ class ErrorLogTest extends BaseHttpTest {
 				.matches("\\[(\\d{4}-\\d{2}-\\d{2}) (\\d{2}:\\d{2}:\\d{2})\\+\\d{4}] \\[pid (\\d+)] \\[client ([0-9a-fA-F:.]+)(:\\d)*] java.lang.RuntimeException");
 	}
 
-	@Test
-	void errorLogCustomFormat() {
-		disposableServer = createServer()
+	@ParameterizedTest
+	@MethodSource("dataCompatibleCombinations")
+	void errorLogCustomFormat(HttpServer httpServer, HttpClient httpClient) {
+		disposableServer = httpServer
 				.handle((req, resp) -> {
 					resp.withConnection(
 							conn -> conn.channel()
@@ -107,7 +123,7 @@ class ErrorLogTest extends BaseHttpTest {
 				)
 				.bindNow();
 
-		getHttpClientResponse("/example/test");
+		getHttpClientResponse(httpClient, "/example/test");
 
 		Mockito.verify(mockedAppender, Mockito.times(1))
 				.doAppend(loggingEventArgumentCaptor.capture());
@@ -120,9 +136,10 @@ class ErrorLogTest extends BaseHttpTest {
 				.isEqualTo("method=GET, uri=/example/test");
 	}
 
-	@Test
-	void secondCallToErrorLogOverridesPreviousOne() {
-		disposableServer = createServer()
+	@ParameterizedTest
+	@MethodSource("dataCompatibleCombinations")
+	void secondCallToErrorLogOverridesPreviousOne(HttpServer httpServer, HttpClient httpClient) {
+		disposableServer = httpServer
 				.handle((req, resp) -> {
 					resp.withConnection(
 							conn -> conn.channel()
@@ -142,16 +159,17 @@ class ErrorLogTest extends BaseHttpTest {
 				.errorLog(false)
 				.bindNow();
 
-		getHttpClientResponse("/example/test");
+		getHttpClientResponse(httpClient, "/example/test");
 
 		Mockito.verify(mockedAppender, Mockito.times(0))
 				.doAppend(loggingEventArgumentCaptor.capture());
 		assertThat(loggingEventArgumentCaptor.getAllValues()).isEmpty();
 	}
 
-	@Test
-	void errorLogFilteringAndFormatting() {
-		disposableServer = createServer()
+	@ParameterizedTest
+	@MethodSource("dataCompatibleCombinations")
+	void errorLogFilteringAndFormatting(HttpServer httpServer, HttpClient httpClient) {
+		disposableServer = httpServer
 				.handle((req, resp) -> {
 					resp.withConnection(
 							conn -> conn.channel()
@@ -169,8 +187,8 @@ class ErrorLogTest extends BaseHttpTest {
 				)
 				.bindNow();
 
-		getHttpClientResponse("/example/test");
-		getHttpClientResponse("/filtered/test");
+		getHttpClientResponse(httpClient, "/example/test");
+		getHttpClientResponse(httpClient, "/filtered/test");
 
 		Mockito.verify(mockedAppender, Mockito.times(1)).doAppend(loggingEventArgumentCaptor.capture());
 		assertThat(loggingEventArgumentCaptor.getAllValues()).hasSize(1);
@@ -182,9 +200,9 @@ class ErrorLogTest extends BaseHttpTest {
 				.isEqualTo("method=GET, uri=/filtered/test");
 	}
 
-	private void getHttpClientResponse(String uri) {
+	private void getHttpClientResponse(HttpClient httpClient, String uri) {
 		try {
-			createClient(disposableServer.port())
+			httpClient.port(disposableServer.port())
 					.get()
 					.uri(uri)
 					.response()
@@ -193,5 +211,65 @@ class ErrorLogTest extends BaseHttpTest {
 		catch (Exception e) {
 			// ignore
 		}
+	}
+
+	static Object[][] dataCompatibleCombinations() throws Exception {
+		SelfSignedCertificate cert = new SelfSignedCertificate();
+		Http11SslContextSpec serverCtxHttp11 = Http11SslContextSpec.forServer(cert.certificate(), cert.privateKey());
+		Http11SslContextSpec clientCtxHttp11 =
+				Http11SslContextSpec.forClient()
+						.configure(builder -> builder.trustManager(InsecureTrustManagerFactory.INSTANCE));
+		Http2SslContextSpec serverCtxHttp2 = Http2SslContextSpec.forServer(cert.certificate(), cert.privateKey());
+		Http2SslContextSpec clientCtxHttp2 =
+				Http2SslContextSpec.forClient()
+						.configure(builder -> builder.trustManager(InsecureTrustManagerFactory.INSTANCE));
+
+		HttpServer _server = createServer().httpRequestDecoder(spec -> spec.h2cMaxContentLength(256));
+
+		HttpServer[] servers = new HttpServer[]{
+				_server, // by default protocol is HTTP/1.1
+				_server.protocol(HttpProtocol.H2C),
+				_server.protocol(HttpProtocol.HTTP11, HttpProtocol.H2C),
+				_server.secure(spec -> spec.sslContext(serverCtxHttp11)), // by default protocol is HTTP/1.1
+				_server.secure(spec -> spec.sslContext(serverCtxHttp2)).protocol(HttpProtocol.H2),
+				_server.secure(spec -> spec.sslContext(serverCtxHttp2)).protocol(HttpProtocol.HTTP11, HttpProtocol.H2)
+		};
+
+		HttpClient _client = HttpClient.create();
+		_client = _client.wiretap(true);
+
+		HttpClient[] clients = new HttpClient[]{
+				_client, // by default protocol is HTTP/1.1
+				_client.protocol(HttpProtocol.H2C),
+				_client.protocol(HttpProtocol.HTTP11, HttpProtocol.H2C),
+				_client.secure(spec -> spec.sslContext(clientCtxHttp11)), // by default protocol is HTTP/1.1
+				_client.secure(spec -> spec.sslContext(clientCtxHttp2)).protocol(HttpProtocol.H2),
+				_client.secure(spec -> spec.sslContext(clientCtxHttp2)).protocol(HttpProtocol.HTTP11, HttpProtocol.H2)
+		};
+
+		Flux<HttpServer> f1 = Flux.fromArray(servers).concatMap(o -> Flux.just(o).repeat(clients.length - 1));
+		Flux<HttpClient> f2 = Flux.fromArray(clients).repeat(servers.length - 1);
+
+		return Flux.zip(f1, f2)
+				.filter(tuple2 -> {
+					HttpServerConfig serverConfig = tuple2.getT1().configuration();
+					HttpClientConfig clientConfig = tuple2.getT2().configuration();
+					List<HttpProtocol> serverProtocols = Arrays.asList(serverConfig.protocols());
+					List<HttpProtocol> clientProtocols = Arrays.asList(clientConfig.protocols());
+					if (serverConfig.isSecure() != clientConfig.isSecure()) {
+						return false;
+					}
+					else if (serverProtocols.size() == 1 && serverProtocols.get(0) == HttpProtocol.H2C &&
+							clientProtocols.size() == 2) {
+						return false;
+					}
+					return serverProtocols.containsAll(clientProtocols) ||
+							clientProtocols.containsAll(serverProtocols);
+
+				})
+				.map(Tuple2::toArray)
+				.collectList()
+				.block(Duration.ofSeconds(30))
+				.toArray(new Object[0][2]);
 	}
 }
