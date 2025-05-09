62b0871ee2ffc530e75cb2971e0a581465a7e244
Violeta Georgieva <696661+violetagg@users.noreply.github.com>
2025-05-09 18:27:51 +0300

    Remove extra write invocation when `Expect: 100-continue` (#3755)
    Fixes #3752
    
    Signed-off-by: Violeta Georgieva <696661+violetagg@users.noreply.github.com>

--- reactor-netty-http/src/main/java/reactor/netty/http/server/AbstractHttpServerMetricsHandler.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/server/AbstractHttpServerMetricsHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2024 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2021-2025 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -158,8 +158,6 @@ abstract class AbstractHttpServerMetricsHandler extends ChannelDuplexHandler {
 		try {
 			if (msg instanceof HttpResponse) {
 				if (((HttpResponse) msg).status().code() == HttpResponseStatus.CONTINUE.code()) {
-					//"FutureReturnValueIgnored" this is deliberate
-					ctx.write(msg, promise);
 					return;
 				}
 
