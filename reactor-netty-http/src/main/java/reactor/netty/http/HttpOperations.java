31166573fcd49b93d32a53c5e7a24d03f5848ab8
Violeta Georgieva <696661+violetagg@users.noreply.github.com>
2025-10-08 10:28:42 +0300

    Ensure the HTTP/2 upgrade stream inherits the proper state from the original HTTP/1.1 connection when successful upgrade from HTTP/1.1 to HTTP/2 (#3957)
    Signed-off-by: Violeta Georgieva <696661+violetagg@users.noreply.github.com>

--- reactor-netty-http/src/main/java/reactor/netty/http/HttpOperations.java
+++ reactor-netty-http/src/main/java/reactor/netty/http/HttpOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2024 VMware, Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2011-2025 VMware, Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,6 +82,12 @@ public abstract class HttpOperations<INBOUND extends NettyInbound, OUTBOUND exte
 
 	protected HttpOperations(HttpOperations<INBOUND, OUTBOUND> replaced) {
 		super(replaced);
+		this.httpMessageLogFactory = replaced.httpMessageLogFactory;
+		this.statusAndHeadersSent = replaced.statusAndHeadersSent;
+	}
+
+	protected HttpOperations(Connection connection, ConnectionObserver listener, HttpOperations<INBOUND, OUTBOUND> replaced) {
+		super(connection, listener, replaced);
 		this.httpMessageLogFactory = replaced.httpMessageLogFactory;
 		this.statusAndHeadersSent = replaced.statusAndHeadersSent;
 	}
